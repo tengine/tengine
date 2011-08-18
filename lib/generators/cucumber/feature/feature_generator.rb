238bc7c46515ce494d2edb3da25c8d836f92ce8c
t-akima <akima@cloud-dev-6.local>
2011-08-18 19:47:15 +0900

    [ユースケースからフィーチャを作成できることを確認する] 日本語のフィーチャファイルとそのstep定義も生成するようにcucumber:featureジェネレータを変更します

--- lib/generators/cucumber/feature/feature_generator.rb
+++ lib/generators/cucumber/feature/feature_generator.rb
@@ -7,21 +7,23 @@ module Cucumber
     argument :fields, :optional => true, :type => :array, :banner => "[field:type, field:type]"
 
     attr_reader :named_args
-  
+
     def parse_fields
       @named_args = @fields.nil? ? [] : @fields.map { |arg| NamedArg.new(arg) }
-    end    
+    end
 
     def generate
       empty_directory 'features/step_definitions'
       name_space = class_name.underscore.split('/')[0..-2].join('/')
-      template 'feature.erb', "features/#{name_space}/manage_#{plural_name}.feature"
-      template 'steps.erb', "features/step_definitions/#{name_space}/#{singular_name}_steps.rb"
+      template 'feature.erb'   , "features/#{name_space}/manage_#{plural_name}.feature"
+      template 'feature_ja.erb', "features/#{name_space}/manage_#{plural_name}_ja.feature"
+      template 'steps.erb'   , "features/step_definitions/#{name_space}/#{singular_name}_steps.rb"
+      template 'steps_ja.erb', "features/step_definitions/#{name_space}/#{singular_name}_steps_ja.rb"
       gsub_file 'features/support/paths.rb', /'\/'/mi do |match|
         "#{match}\n    when /the new #{table_name.singularize} page/\n      new_#{table_name.singularize}_path\n"
       end
     end
-  
+
     def self.banner
       "#{$0} cucumber:feature ModelName [field:type, field:type]"
     end
