e414ebc5e6a1ceb5f50abb89cd280ee4ebfb5658
t-akima <akima@cloud-dev-6.local>
2011-08-18 13:35:21 +0900

    [ユースケースからフィーチャを作成できることを確認する] デフォルトのrails g scaffoldで生成されるコントローラ、ビューで使用されるインスタンス変数名がTengine::Core::に当たるtengine_core_を含んでいるので単にdriverとなるように修正しました

--- app/controllers/tengine/core/drivers_controller.rb
+++ app/controllers/tengine/core/drivers_controller.rb
@@ -2,50 +2,50 @@ class Tengine::Core::DriversController < ApplicationController
   # GET /tengine/core/drivers
   # GET /tengine/core/drivers.json
   def index
-    @tengine_core_drivers = Tengine::Core::Driver.all
+    @drivers = Tengine::Core::Driver.all(:sort => [[:_id]]).page(params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @tengine_core_drivers }
+      format.json { render json: @drivers }
     end
   end
 
   # GET /tengine/core/drivers/1
   # GET /tengine/core/drivers/1.json
   def show
-    @tengine_core_driver = Tengine::Core::Driver.find(params[:id])
+    @driver = Tengine::Core::Driver.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @tengine_core_driver }
+      format.json { render json: @driver }
     end
   end
 
   # GET /tengine/core/drivers/new
   # GET /tengine/core/drivers/new.json
   def new
-    @tengine_core_driver = Tengine::Core::Driver.new
+    @driver = Tengine::Core::Driver.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @tengine_core_driver }
+      format.json { render json: @driver }
     end
   end
 
   # GET /tengine/core/drivers/1/edit
   def edit
-    @tengine_core_driver = Tengine::Core::Driver.find(params[:id])
+    @driver = Tengine::Core::Driver.find(params[:id])
   end
 
   # POST /tengine/core/drivers
   # POST /tengine/core/drivers.json
   def create
-    @tengine_core_driver = Tengine::Core::Driver.new(params[:tengine_core_driver])
+    @driver = Tengine::Core::Driver.new(params[:driver])
 
     respond_to do |format|
       if @tengine_core_driver.save
-        format.html { redirect_to @tengine_core_driver, notice: 'Driver was successfully created.' }
-        format.json { render json: @tengine_core_driver, status: :created, location: @tengine_core_driver }
+        format.html { redirect_to @driver, notice: 'Driver was successfully created.' }
+        format.json { render json: @driver, status: :created, location: @driver }
       else
         format.html { render action: "new" }
         format.json { render json: @tengine_core_driver.errors, status: :unprocessable_entity }
@@ -56,11 +56,11 @@ class Tengine::Core::DriversController < ApplicationController
   # PUT /tengine/core/drivers/1
   # PUT /tengine/core/drivers/1.json
   def update
-    @tengine_core_driver = Tengine::Core::Driver.find(params[:id])
+    @driver = Tengine::Core::Driver.find(params[:id])
 
     respond_to do |format|
-      if @tengine_core_driver.update_attributes(params[:tengine_core_driver])
-        format.html { redirect_to @tengine_core_driver, notice: 'Driver was successfully updated.' }
+      if @tengine_core_driver.update_attributes(params[:driver])
+        format.html { redirect_to @driver, notice: 'Driver was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
@@ -72,7 +72,7 @@ class Tengine::Core::DriversController < ApplicationController
   # DELETE /tengine/core/drivers/1
   # DELETE /tengine/core/drivers/1.json
   def destroy
-    @tengine_core_driver = Tengine::Core::Driver.find(params[:id])
+    @driver = Tengine::Core::Driver.find(params[:id])
     @tengine_core_driver.destroy
 
     respond_to do |format|
