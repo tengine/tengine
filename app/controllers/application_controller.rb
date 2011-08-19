8ef12dce867ddc392e8b8bf18486723423db6da9
t-akima <akima@cloud-dev-6.local>
2011-08-19 11:41:16 +0900

    [ユースケースからフィーチャを作成できることを確認する] テスト対象となる画面は日本語で作成することにしたので、 http://guides.rubyonrails.org/i18n.html を参考にI18n.localeの操作ができるようにしました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  def set_locale
+    if params[:locale]
+      session[:session_locale] = params[:locale]
+    end
+    I18n.locale = session[:session_locale] || I18n.default_locale
+  end
+
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# I18nについては以下を参照してください。
+# see http://guides.rubyonrails.org/i18n.html
+
+# tell the I18n library where to find your translations
+# I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+# set default locale to something other than :en
+I18n.default_locale = :ja
