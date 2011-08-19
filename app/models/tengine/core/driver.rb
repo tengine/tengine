cae2cc4925c92f728f97727b2ddabcd5722ebde8
t-akima <akima@cloud-dev-6.local>
2011-08-19 19:16:48 +0900

    [ユースケースからフィーチャを作成できることを確認する] バリデーションのメッセージのヘッダーをja.ymlから取得するように変更しました。これに伴い英語のメッセージも取得できるように英語のメッセージリソースを https://github.com/svenfuchs/rails-i18n/blob/master/rails/locale/en-US.yml からコピーしました

--- app/models/tengine/core/driver.rb
+++ app/models/tengine/core/driver.rb
@@ -3,4 +3,7 @@ class Tengine::Core::Driver
   field :name, :type => String
   field :version, :type => String
   field :enabled, :type => Boolean
+
+  validates :name, :presence => true
+  validates :version, :presence => true
 end
