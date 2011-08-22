02726a13cfcccbabb6952b9bb84e114041864c53
t-akima <akima@cloud-dev-6.local>
2011-08-22 18:02:30 +0900

    [ユースケースからフィーチャを作成できることを確認する] rake cucumberで失敗していたので修正しました。

--- features/usecases/コア/exception_raiser_dsl.rb
+++ features/usecases/コア/exception_raiser_dsl.rb
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-require 'tengine/core'
+# require 'tengine/core'
 
-class Tengine::TestException < StandardError
-end
+# class Tengine::TestException < StandardError
+# end
 
-Tengine.driver :driver1 do
-  on:foo do
-    raise Tengine::TestException, "exception at driver1.foo"
-  end
+# Tengine.driver :driver1 do
+#   on:foo do
+#     raise Tengine::TestException, "exception at driver1.foo"
+#   end
 
-  # 有効／無効について何も記述がないのでデフォルトで起動後に有効になります
+#   # 有効／無効について何も記述がないのでデフォルトで起動後に有効になります
 
-end
+# end
