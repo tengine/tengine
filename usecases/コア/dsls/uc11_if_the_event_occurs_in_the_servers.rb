865f886855c6b94448e39070ce82b9dc4f599924
Takahisa Tokunaga <tokunaga.jp@gmail.com>
2011-08-29 18:14:55 +0900

    [ユースケース*] ユースケースで利用するDSLを追加しました。

---
+++ usecases/コア/dsls/uc11_if_the_event_occurs_in_the_servers.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+require 'tengine/core'
+
+Tengine.driver :driver11 do
+
+  # サーバAかBかCからのイベントが発生した場合のみ処理を実行する
+  on :event11.in(/^res:server_[abc]$/) do
+    puts "handler11"
+  end
+
+end
