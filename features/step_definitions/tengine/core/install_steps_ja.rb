04efd0b6777a4c92b47fdd6391ab4dd9c4c5c89e
g-morita <g-morita@ec-one.com>
2011-08-30 22:19:35 +0900

    [アプリケーション開発者が開発環境へインストールする.featureを作成] 中途半端ですが一旦コミットします。

---
+++ features/step_definitions/tengine/core/install_steps_ja.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+前提 /^OSの種類は"([^"]*)"である$/ do |arg1|
+  # pending # express the regexp above with the code you wish you had
+end
+
+前提 /^OSのバージョンは"([^"]*)"である$/ do |arg1|
+  # pending # express the regexp above with the code you wish you had
+end
+
+前提 /^OSのビット幅は"([^"]*)"である$/ do |arg1|
+  # pending # express the regexp above with the code you wish you had
+end
+
+前提 /^"([^"]*)"のバージョンは"([^"]*)"である$/ do |pkg, version|
+  puts "#{pkg}は#{version}です"
+  # pending # express the regexp above with the code you wish you had
+end
