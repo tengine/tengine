1eb0028956bf41fbe99e8f882bed1d49028e5012
t-akima <akima@cloud-dev-6.local>
2011-09-02 21:30:12 +0900

    [イベントハンドラストアへのイベントハンドラの登録を行う] テストに使用するDSLがユースケースで使用しているものと全く同一であることを確認できるようにspec内に書くのではなくファイルをコピーしました。

--- spec/models/tengine/core/dsl_loader_spec.rb
+++ spec/models/tengine/core/dsl_loader_spec.rb
@@ -5,45 +5,11 @@ require 'tempfile'
 describe Tengine::Core::DslLoader do
 
   describe :evaluate do
-    before :all do
-      @temp_dir = Dir.tmpdir
-      @temp_dsl_01 = Tempfile::new("temp_dsl_01.rb", @temp_dir)
-      @temp_dsl_01.puts(<<-EOS)
-# -*- coding: utf-8 -*-
-require 'tengine/core'
-Tengine.driver :driver01 do
-  # イベントに対応する処理の実行する
-  on:event01 do
-    puts "handler01"
-  end
-end
-      EOS
-      @temp_dsl_02 = Tempfile::new("temp_dsl_02.rb", @temp_dir)
-      @temp_dsl_02.puts(<<-EOS)
-# -*- coding: utf-8 -*-
-require 'tengine/core'
-Tengine.driver :driver02 do
-  on:event02_1 do
-    puts "handler02_1"
-    fire(:event02_2)
-  end
-  on:event02_2 do
-    puts "handler02_2"
-  end
-end
-      EOS
-    end
-
-    after :all do
-      @temp_dsl_01.close(true)
-      @temp_dsl_02.close(true)
-    end
-
     context "イベントハンドラ定義のファイル名まで指定されている" do
       before do
         @config = {
-          :dsl_store_path => @temp_dir,
-          :dsl_file => @temp_dsl_01.path,
+          :dsl_store_path => File.expand_path('dsls'),
+          :dsl_file => File.expand_path('dsls/uc01_execute_processing_for_event.rb', File.dirname(__FILE__)),
         }
         @loader = Tengine::Core::DslEnv.new(@config)
         @loader.extend(Tengine::Core::DslLoader)
---
+++ spec/models/tengine/core/dsls/uc40_generated_handler.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+require 'tengine/core'
+
+Tengine.driver :driver40 do
+
+  # イベントに対応する処理の実行する
+  on:event40 do
+    # イベントに対応する処理を記述してください
+    puts "Hello Tengine World."
+  end
+
+end
