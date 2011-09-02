721622a337f63a886714b689e8e231071fbbb689
Takahisa Tokunaga <tokunaga.jp@gmail.com>
2011-09-02 10:10:12 +0900

    [イベントハンドラストアへのイベントハンドラの登録を行う] イベントハンドラ定義ファイルのロードまで

--- app/models/tengine/core/dsl_binder.rb
+++ app/models/tengine/core/dsl_binder.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 module Tengine::Core::DslBinder
   def evaluate
+    $:.unshift @config[:dsl_store_path]
   end
 end
--- spec/models/tengine/core/dsl_binder_spec.rb
+++ spec/models/tengine/core/dsl_binder_spec.rb
@@ -2,4 +2,23 @@
 require 'spec_helper'
 
 describe Tengine::Core::DslBinder do
+
+  describe :evaluate do
+    before do
+      @config = {
+        :dsl_store_path => File::expand_path("../../../../usecases/コア/dsls/"),
+        :dsl_file => "uc01_execute_processing_for_event.rb",
+      }
+      @binder = Tengine::Core::DslEnv.new(@config)
+      @binder.extend(Tengine::Core::DslBinder)
+    end
+
+    describe "イベントハンドラ定義ストア" do
+      it "ディレクトリがLOAD_PATHに追加されていること" do
+        @binder.evaluate
+        $LOAD_PATH.include?(@config[:dsl_store_path]).should be_true
+      end
+    end
+  end
+
 end
