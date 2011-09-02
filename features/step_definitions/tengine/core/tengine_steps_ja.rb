b9cdbdde5e9abf74328868a822dc016ca5eaed50
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-09-02 11:59:41 +0900

    [アプリケーション開発者が開発環境へインストールする.featureを作成]正常系のシナリオで使用するステップ定義を実装するために、以下のコマンドを実行して出力されたステップ定義を一旦tengine_steps_ja.rbに貼り付けました。
    cucumber --require features features/usecases/コア/アプリケーション開発者が開発環境へインストールする/アプリケーション開発者が開発環境で接続テストを行いTengineコア、Tengineコンソールを起動する.feature -n "アプリケーション開発者が開発環境で接続テストを行いTengineコア、Tengineコンソールを起動する_正常系"
    
    引き続きpending箇所の実装を行います

--- features/step_definitions/tengine/core/tengine_steps_ja.rb
+++ features/step_definitions/tengine/core/tengine_steps_ja.rb
@@ -1,1 +1,61 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+前提 /^"([^"]*)"パッケージのインストールおよびセットアップが完了している$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+前提 /^"([^"]*)"のインストールおよびセットアップが完了している$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+前提 /^"([^"]*)"プロセスが起動している$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+前提 /^"([^"]*)"プロセスが停止している$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"([^"]*)"を行うために"([^"]*)"というコマンドを実行する$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^"([^"]*)"の標準出力に"([^"]*)"と出力されていること$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"([^"]*)"を起動するために"([^"]*)"というコマンドを実行する$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^"([^"]*)"の標準出力からPIDを確認することができること$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^"([^"]*)"が起動していることを"([^"]*)"で確認できること$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"([^"]*)"が起動していることを"([^"]*)"で確認できること$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"([^"]*)"に"([^"]*)"を入力する$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^"([^"]*)"に以下の行が表示されること$/ do |arg1, table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"([^"]*)"プロセスを Ctl\+c で停止する$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^"([^"]*)"が停止していることを"([^"]*)"で確認できること$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^"([^"]*)"を Ctl\+c で停止する$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
