b5a092367862587010d748136d0cfb0d097cf5a5
Takahisa Tokunaga <tokunaga.jp@gmail.com>
2011-09-02 10:10:12 +0900

    [デプロイヤの設計と実装] tengine をデプロイする先のサーバが scm を利用できるとは限らないので、開発環境でソースコードを固めてサーバへコピーするようにしました。

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,9 @@ server "192.168.1.54", :web, :app, :mq, :db
 set :use_sudo,    false
 set :user,        'root'
 set :password,    'password'
+set :ssh_options, {
+  :forward_agent => true
+}
 
 
 #####################
@@ -30,12 +33,6 @@ set :password,    'password'
 set :scm, :git
 set :scm_verbose, true
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`, and `file://`
-# set :scm_user do
-#   Capistrano::CLI.ui.ask("scm user:")
-# end
-# set :scm_password do
-#   Capistrano::CLI.ui.ask("scm pass:")
-# end
 
 set :repository,  'ssh://git@cloud-dev.ec-one.com/tengine_console.git'  # rubyセンターへ変更予定
 set :branch,      'develop'
@@ -123,6 +120,31 @@ namespace :deploy do
 
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "[internal][override] This is called by update_code"
+  task :finalize_update, :except => { :no_release => true } do
+    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+    # mkdir -p is making sure that the directories are there for some SCM's that don't
+    # save empty folders
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+      mkdir -p #{latest_release}/public &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
+      ln -s #{shared_path}/system #{latest_release}/public/system &&
+      ln -s #{shared_path}/pids #{latest_release}/tmp/pids
+    CMD
+
+    # rails3.1だとimageファイル系のファイルの配置が変わってるので上書きします
+    # 以下を $RAIL_ROOT/app/assets を指定するようにしてもよいですが、まずは deploy 毎に最新取得する対象にしておきます。
+    #
+    # if fetch(:normalize_asset_timestamps, true)
+    #   stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+    #   asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+    #   run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+    # end
   end
 end
 
