b2d1c562403426ae1a673d6547ad3ff7a17ab184
秋間 武志 <akima@akima-no-MacBook-Air.local>
2011-09-04 20:46:16 +0900

    [イベントハンドラストアへのイベントハンドラの登録を行う] Tengine::Core::Handlerのcreate時にTengine::Core::HandlerPathへ更新する機能のテストを追加しました。

--- spec/models/tengine/core/driver_spec.rb
+++ spec/models/tengine/core/driver_spec.rb
@@ -1,5 +1,23 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Tengine::Core::Driver do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "保存時にHandlerPathを自動的に登録します" do
+    before do
+      Tengine::Core::Driver.delete_all
+      Tengine::Core::HandlerPath.delete_all
+      @d11 = Tengine::Core::Driver.new(name:"driver1", version:"1", enabled:true)
+      @d11h1 = @d11.handlers.new(:event_type_names => ["foo"])
+      @d11h2 = @d11.handlers.new(:event_type_names => ["boo"])
+      @d11h3 = @d11.handlers.new(:event_type_names => ["blah"])
+      @d11.save!
+    end
+    it do
+      Tengine::Core::HandlerPath.count.should == 3
+      Tengine::Core::HandlerPath.event_type_name("foo").map(&:handler_id).should == [@d11h1.id]
+      Tengine::Core::HandlerPath.event_type_name("boo").map(&:handler_id).should == [@d11h2.id]
+      Tengine::Core::HandlerPath.event_type_name("blah").map(&:handler_id).should == [@d11h3.id]
+    end
+  end
+
 end
