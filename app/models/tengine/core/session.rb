da0335f517c58e25eaa6421209a8a98c14e446da
秋間 武志 <akima@akima-no-MacBook-Air.local>
2011-09-04 21:10:22 +0900

    [イベントハンドラストアへのイベントハンドラの登録を行う] Tengine::Core::Session#[]と#[]=でpropertiesを参照、変更できるようにしました。

--- app/models/tengine/core/session.rb
+++ app/models/tengine/core/session.rb
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 class Tengine::Core::Session
   include Mongoid::Document
   field :properties, :type => Hash
   map_yaml_accessor :properties
 
   belongs_to :driver, :index => true, :class_name => "Tengine::Core::Driver"
+
+  # 元々の[]と[]=メソッドをオーバーライドしているので要注意
+  def [](key); properties[key]; end
+  def []=(key, value); properties[key] = value; end
+
 end
--- spec/models/tengine/core/session_spec.rb
+++ spec/models/tengine/core/session_spec.rb
@@ -20,5 +20,18 @@ describe Tengine::Core::Session do
       subject.properties[:key5].should == {:nested => "hash"}
       subject.properties[:key6].should == :symbol_value
     end
+    it "should allow to read properties by using []" do
+      subject["key1"].should == 100
+      subject["key2"].should == "string value"
+      subject["key3"].should == Time.utc(2011,9,4,20,58)
+      subject[:key4].should == [:array, "of", "variables", true, false, nil, 99.9999]
+      subject[:key5].should == {:nested => "hash"}
+      subject[:key6].should == :symbol_value
+    end
+    it "should allow to write properties by using []=" do
+      subject["key1"] = 99
+      subject["key1"].should == 99
+    end
   end
+
 end
