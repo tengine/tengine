49cae2ed9d6a189872c714b6e755b92271796ade
g-morita <g-morita@ec-one.com>
2011-09-05 10:46:44 +0900

    [fアプリケーション開発者が開発環境へインストールする.featureを作成] プロセスの起動、確認、停止のstep定義を修正しました。

--- features/step_definitions/tengine/core/tengine_steps_ja.rb
+++ features/step_definitions/tengine/core/tengine_steps_ja.rb
@@ -15,16 +15,14 @@ end
 #  pending # express the regexp above with the code you wish you had
 end
 
-
 もし /^"([^"]*)"を行うために"([^"]*)"というコマンドを実行する$/ do |name, command|
-  @stdout = ""
+  @io = IO.popen(command)
+end
+
+もし /^"([^"]*)"の起動を行うために"([^"]*)"というコマンドを実行する$/ do |name, command|
   io = IO.popen(command)
-  while line = io.gets
-    if line.empty? then
-      break
-    end
-    @stdout << line
-  end
+  @h = {} 
+  @h[name] = {:io => io}
 end
 
 ならば /^"([^"]*)"の標準出力に"([^"]*)"と出力されていること$/ do |name, stdout|
@@ -32,14 +30,33 @@ end
 end
 
 ならば /^"([^"]*)"の標準出力からPIDを確認することができること$/ do |name|
-  pending # Tengineコアをフォアグラウンド起動した際に標準出力が決まっていないので、PIDの取得部分は暫定的に正規表現で数値を引っこ抜いている
-  @pid = @stdout.match(/\d+/)[0]
-  @pid.should_not be_empty
+  # TODO Tengineコアをフォアグラウンド起動した際に標準出力が決まっていないので、PIDの取得部分は暫定的に正規表現で数値を引っこ抜いている
+  pid_regexp = /PID:(\d+)/
+  while line = @h[name][:io].gets
+    if line.match(pid_regexp) then
+      pid = line.match(pid_regexp)[1]
+      @h[name][:pid] = pid
+      break
+    end
+  end
 end
 
-ならば /^"([^"]*)"が起動していることを"([^"]*)"で確認できること$/ do |name, command|
-  result = `#{command.gsub(/PID/, @pid)}`
-  result.should match(/#{@pid}/)
+ならば /^"([^"]*)"が起動していることをPIDを用いて"([^"]*)"というコマンドで確認できること$/ do |name,  command|
+  pid = @h[name][:pid]
+  exec_command = "#{command.gsub(/PID/, pid)} > /dev/null"
+  system(exec_command).should be_true
+end
+
+ならば /^"([^"]*)"が停止していることをPIDを用いて"([^"]*)"というコマンドで確認できること$/ do |name,  command|
+  pid = @h[name][:pid]
+  exec_command = "#{command.gsub(/PID/, pid)} > /dev/null"
+  system(exec_command).should be_false
+end
+
+もし /^"([^"]*)"を Ctl\+c で停止する$/ do |name|
+  pid = @h[name][:pid]
+  exec_command = "kill -2 #{pid} > /dev/null"
+  system(exec_command)
 end
 
 もし /^"([^"]*)"が起動していることを"([^"]*)"で確認できる$/ do |arg1, arg2|
@@ -55,14 +72,3 @@ end
   pending # express the regexp above with the code you wish you had
 end
 
-もし /^"([^"]*)"プロセスを Ctl\+c で停止する$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-ならば /^"([^"]*)"が停止していることを"([^"]*)"で確認できること$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-もし /^"([^"]*)"を Ctl\+c で停止する$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
