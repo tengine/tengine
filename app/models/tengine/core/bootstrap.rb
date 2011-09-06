77e017c31bc7e699a6e106f2a4748bcb15c36cd7
Hirotake Fuchigami <taigou@gmail.com>
2011-09-06 23:49:28 +0900

    [デーモンプロセス設計と実装]接続テストの実装を行いました

--- app/models/tengine/core/bootstrap.rb
+++ app/models/tengine/core/bootstrap.rb
@@ -2,7 +2,7 @@
 
 class Tengine::Core::Bootstrap
 
-  attr_reader :config
+  attr_accessor :config
 
   def initialize(hash)
     @config = Tengine::Core::Config.new(hash)
@@ -15,6 +15,10 @@ class Tengine::Core::Bootstrap
       load_dsl unless config[:tengined][:prevent_loader] == true
       start_kernel
     when "test"
+      # TODO 接続テスト用イベントハンドラ定義ファイルをload_pathに指定してあげる必要があります
+      # config[:tengined][:load_path] = File.expand_path("../../../../lib/tengine/core/connection_test/fire_bar_on_foo.rb", __FILE__)
+      # @config[:tengined][:load_path] = tengine_coreのルート/lib/tengine/core/connection_test/fire_bar_on_foo.rb
+      # tengine_coreのルート/lib/tengine/core/connection_test/VERSION
       load_dsl
       start_kernel
       start_connection_test
@@ -39,8 +43,18 @@ class Tengine::Core::Bootstrap
   end
 
   def enable_drivers
+    drivers = Tengine::Core::Driver.where(:version => config.dsl_version, :enabled_on_activation => false)
+    drivers.each{ |d| d.update_attribute(:enabled, true) }
   end
 
   def start_connection_test
+    event_type_name = :foo
+    options = { :notification_level_key => :info }
+    Tengine::Event.config = {
+      :connection => config[:event_queue][:conn],
+      :exchange => config[:event_queue][:exchange],
+      :queue => config[:event_queue][:queue]
+    }
+    Tengine::Event.fire(event_type_name, options)
   end
 end
--- spec/models/tengine/core/bootstrap_spec.rb
+++ spec/models/tengine/core/bootstrap_spec.rb
@@ -132,6 +132,49 @@ describe "Tengine::Core::Bootstrap" do
   end
 
   describe :enable_drivers do
+    before do
+      Tengine::Core::Driver.delete_all
+      t = Time.local(2011,9,5,17,28,30)
+      Time.stub!(:now).and_return(t)
 
+      @d1 = Tengine::Core::Driver.create!(:name=>"driver1", :version=>"20110905172830", :enabled=>false, :enabled_on_activation=>false)
+      @d2 = Tengine::Core::Driver.create!(:name=>"driver2", :version=>"20110905172830", :enabled=>false, :enabled_on_activation=>false)
+      @d3 = Tengine::Core::Driver.create!(:name=>"driver3", :version=>"20110905172830", :enabled=>false, :enabled_on_activation=>false)
+    end
+
+    it "enable_driversで、enabled=trueになる" do
+      options = {
+        :action => "enable",
+        :tengined => {
+          :load_path => "./"
+        }
+      }
+      bootstrap = Tengine::Core::Bootstrap.new(options)
+      bootstrap.boot
+
+      Tengine::Core::Driver.where(:version => "20110905172830") do |d|
+        d.enabled.should be_true
+      end
+    end
+  end
+
+  describe :start_connection_test do
+    it "イベントキューにイベントを発火 " do
+      options = {
+        :action => "test",
+      }
+
+      bootstrap = Tengine::Core::Bootstrap.new(options)
+      Tengine::Event.should_receive(:fire).with(:foo, :notification_level_key => :info)
+      event = bootstrap.start_connection_test
+
+      Tengine::Event.config[:connection][:host].should == "localhost"
+      Tengine::Event.config[:connection][:port].should == 5672
+      Tengine::Event.config[:exchange][:name].should == "tengine_event_exchange"
+      Tengine::Event.config[:exchange][:type].should == "direct"
+      Tengine::Event.config[:exchange][:durable].should be_true
+      Tengine::Event.config[:queue][:name].should == "tengine_event_queue"
+      Tengine::Event.config[:queue][:durable].should be_true
+    end
   end
 end
