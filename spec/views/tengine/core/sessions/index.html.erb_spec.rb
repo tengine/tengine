9e5c3658b5c573881b304c6cb8a9c8b289956494
t-akima <akima@cloud-dev-6.local>
2011-09-10 11:01:24 +0900

    [フィルターの詳細設計と実装] Tengine::Core::Sessionにlock_versionとsystem_propertiesを追加しました

--- spec/views/tengine/core/sessions/index.html.erb_spec.rb
+++ spec/views/tengine/core/sessions/index.html.erb_spec.rb
@@ -4,10 +4,14 @@ describe "tengine/core/sessions/index.html.erb" do
   before(:each) do
     mock_pagination(assign(:sessions, [
       stub_model(Tengine::Core::Session,
-        :properties => {"a"=>"1", "b"=>"2"}
+        :lock_version => 1,
+        :properties => {"a"=>"1", "b"=>"2"},
+        :system_properties => {"c"=>"1", "d"=>"2"}
       ),
       stub_model(Tengine::Core::Session,
-        :properties => {"a"=>"1", "b"=>"2"}
+        :lock_version => 1,
+        :properties => {"a"=>"1", "b"=>"2"},
+        :system_properties => {"c"=>"1", "d"=>"2"}
       )
     ]))
   end
@@ -15,6 +19,10 @@ describe "tengine/core/sessions/index.html.erb" do
   it "renders a list of tengine_core_sessions" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td>pre", :text => CGI.escapeHTML(YAML.dump({"a"=>"1", "b"=>"2"})), :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td>pre", :text => CGI.escapeHTML(YAML.dump({"c"=>"1", "d"=>"2"})), :count => 2
   end
 end
