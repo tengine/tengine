0f16f7045e313b1c91c68e7a5f9afa2633adf61d
g-morita <g-morita@ec-one.com>
2011-09-12 18:41:51 +0900

    [Tengineコンソールの詳細設計・実装] Tengine::Event, Tengine::Core::Event の属性名が変更となったので修正しました。画面のテストが落ちていたので修正しましたが、後２つテストが落ちてしまいます。中途半端ですが一旦コミットします。
    Failures:
    
      1) Tengine::Core::EventsController GET index assigns all tengine_core_events as @tengine_core_events
         Failure/Error: events.to_a.should eq([event])
           
           expected [#<Tengine::Core::Event _id: 4e6dd2f347d17c6c6000001e, _type: nil, event_type_name: nil, key: nil, source_name: nil, occurred_at: nil, level: 1, confirmed: nil, sender_name: nil, properties: nil>]
                got []
           
           (compared using ==)
           
           Diff:
           @@ -1,2 +1,2 @@
           -[#<Tengine::Core::Event _id: 4e6dd2f347d17c6c6000001e, _type: nil, event_type_name: nil, key: nil, source_name: nil, occurred_at: nil, level: 1, confirmed: nil, sender_name: nil, properties: nil>]
           +[]
         # ./spec/controllers/tengine/core/events_controller_spec.rb:37:in `block (3 levels) in <top (required)>'
    
      2) tengine/core/events/index.html.erb renders a list of tengine_core_events
         Failure/Error: render
         ActionView::Template::Error:
           undefined method `enum_base_name' for NilClass:Class
         # ./app/views/tengine/core/events/index.html.erb:28:in `block in _app_views_tengine_core_events_index_html_erb__1672710075473314302_70133367663840'
         # ./app/views/tengine/core/events/index.html.erb:5:in `_app_views_tengine_core_events_index_html_erb__1672710075473314302_70133367663840'
         # ./spec/views/tengine/core/events/index.html.erb_spec.rb:28:in `block (2 levels) in <top (required)>'
    
    Finished in 13.3 seconds
    267 examples, 2 failures
    
    Failed examples:
    
    rspec ./spec/controllers/tengine/core/events_controller_spec.rb:31 # Tengine::Core::EventsController GET index assigns all tengine_core_events as @tengine_core_events
    rspec ./spec/views/tengine/core/events/index.html.erb_spec.rb:27 # tengine/core/events/index.html.erb renders a list of tengine_core_events
    rake aborted!

--- app/models/tengine/core/event/finder.rb
+++ app/models/tengine/core/event/finder.rb
@@ -8,8 +8,8 @@ class Tengine::Core::Event::Finder
   attr_accessor :source_name
   attr_accessor :occurred_at_start
   attr_accessor :occurred_at_end
-  attr_accessor :notification_level_ids
-  attr_accessor :notification_confirmed
+  attr_accessor :level_ids
+  attr_accessor :confirmed
   attr_accessor :sender_name
   attr_accessor :properties
 
@@ -17,12 +17,12 @@ class Tengine::Core::Event::Finder
   attr_accessor :reflesh_interval
   
   # 通知レベル
-  multi_selectable_attr :notification_level, :enum => Tengine::Core::Event.notification_level_enum
+  multi_selectable_attr :level, :enum => Tengine::Core::Event.level_enum
 
 
   def initialize(attrs = {}, page = {})
     attrs = {
-      notification_level_ids: default_notification_level_ids
+      level_ids: default_level_ids
     }.update(attrs || {})
     attrs.each do |attr, v| 
       send("#{attr}=", v) unless v.blank?
@@ -31,9 +31,9 @@ class Tengine::Core::Event::Finder
   end
 
   # デフォルトでは通知レベルがすべて選択された状態にする
-  def default_notification_level_ids
+  def default_level_ids
     result = []
-    Tengine::Core::Event.notification_level_entries.each do |entry|
+    Tengine::Core::Event.level_entries.each do |entry|
       result << entry.id
     end
     return result
@@ -50,8 +50,8 @@ class Tengine::Core::Event::Finder
     result = result.where(source_name: source_name) if source_name
     result = result.where(:occurred_at.gte => occurred_at_start) if occurred_at_start
     result = result.where(:occurred_at.lte =>  occurred_at_end) if occurred_at_end
-    result = result.any_in(notification_level: notification_level_ids) if notification_level_ids
-    result = result.where(notification_confirmed: notification_confirmed) if notification_confirmed
+    result = result.any_in(level: level_ids) if level_ids
+    result = result.where(confirmed: confirmed) if confirmed
     result = result.where(sender_name: sender_name) if sender_name
     result = result.where(properties: properties) if properties
     # ソート
