1412d4f964584d05f31124914d2d0e075c88a9ba
t-akima <akima@nautilus-technologies.com>
2011-09-13 22:49:01 +0900

    [session、eventメソッドの実装] テストを見込んでメソッドを分割しました

--- app/models/tengine/core/session_wrapper.rb
+++ app/models/tengine/core/session_wrapper.rb
@@ -14,20 +14,28 @@ class Tengine::Core::SessionWrapper
     @source.properties[key.to_s]
   end
 
-  def update(properties)
+  def update(properties, &block)
     return if @options[:ignore_update]
-    __update__(:properties, properties)
+    __update__(:properties, properties, &block)
   end
 
-  def system_update(properties)
-    __update__(:system_properties, properties)
+  def system_update(properties, &block)
+    __update__(:system_properties, properties, &block)
   end
 
   private
-  def __update__(target_name, properties)
-    new_vals = @source.send(target_name).merge(properties.stringify_keys)
+  def __update__(target_name, properties, &block)
+    new_vals = __get_properties__(target_name).merge(properties.stringify_keys)
     @source.send("#{target_name}=", new_vals)
     @source.save!
   end
+
+  # テストで同時に値を取得したことを再現するために、
+  # データを取得するメソッドで待ち合わせするフックとなるようにメソッドに分けています
+  def __get_properties__(target_name, reload = false)
+    @source.reload if reload
+    @source.send(target_name)
+  end
+
 
 end
