d8308ca97bd1ba4b32f54be1f542b7e576da248a
t-akima <akima@nautilus-technologies.com>
2011-09-14 00:28:21 +0900

    [session、eventメソッドの実装] リトライに失敗し続けリトライの上限に達してしまった場合のテストと実装を追加しました

--- examples/uc64_safety_countup.rb
+++ examples/uc64_safety_countup.rb
@@ -6,7 +6,7 @@ driver :driver64 do
   session.update(:foo => 100) # ドライバ登録時にそのセッションに キー:foo に対して 値100 を設定する。
 
   on:event64 do
-    session.update(:retry => 1) do |hash|
+    session.update(:retry => 2) do |hash|
       hash[:foo] = hash[:foo] + 1
       # hash[:foo] += 1 # と書いても良い
     end
