3cb3d7769c617064cf425680cc4efe44de506f26
t-akima <akima@nautilus-technologies.com>
2011-09-14 17:14:34 +0900

    [tengine_coreをサンプルアプリで使えるようにする] productionモードで動かなくなっていたので修正しました

--- config/initializers/array_and_hash_support.rb
+++ config/initializers/array_and_hash_support.rb
@@ -1,38 +1,41 @@
 # -*- coding: utf-8 -*-
-require 'rails/generators/generated_attribute'
-require 'generators/rspec'
-class Rails::Generators::GeneratedAttribute
+if Rails.env == "development"
 
-  def field_type_with_array_and_hash_support
-    @field_type ||=
-      case type
-      when :array then :text_field
-      when :hash then :text_area
-      else
-        field_type_without_array_and_hash_support
-      end
-  end
-  alias_method_chain :field_type, :array_and_hash_support
+  require 'rails/generators/generated_attribute'
+  require 'generators/rspec'
 
-  # rspec/lib/generators/rspec.rb を上書き
-  def input_type
-    @input_type ||=
-      case type
-      when :hash, :text then 'textarea'
-      else
-        "input"
-      end
-  end
+  class Rails::Generators::GeneratedAttribute
+    def field_type_with_array_and_hash_support
+      @field_type ||=
+        case type
+        when :array then :text_field
+        when :hash then :text_area
+        else
+          field_type_without_array_and_hash_support
+        end
+    end
+    alias_method_chain :field_type, :array_and_hash_support
 
-  def default_with_array_and_hash_support
-    @default ||=
-      case type
-      when :array then ["abc", "123"]
-      when :hash then {"a"=>"1", "b"=>"2"}
-      else
-        default_without_array_and_hash_support
-      end
+    # rspec/lib/generators/rspec.rb を上書き
+    def input_type
+      @input_type ||=
+        case type
+        when :hash, :text then 'textarea'
+        else
+          "input"
+        end
+    end
+
+    def default_with_array_and_hash_support
+      @default ||=
+        case type
+        when :array then ["abc", "123"]
+        when :hash then {"a"=>"1", "b"=>"2"}
+        else
+          default_without_array_and_hash_support
+        end
+    end
+    alias_method_chain :default, :array_and_hash_support
   end
-  alias_method_chain :default, :array_and_hash_support
 
 end
