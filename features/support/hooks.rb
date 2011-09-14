214a881e6b78c7b3d09d2a755c2514183c3f184e
y-noguchi <y-noguchi@cloud-dev-2.local>
2011-09-14 22:00:31 +0900

    [アプリケーション開発者がTengineコアのイベントハンドラ定義を試してみる.featureのstep定義を修正する]シナリオ起動前に、DBをクリーンするようにしました

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,10 @@
+Before do |s|
+  `touch ~/test_before1`
+  IO.popen("ps -e |grep \"mongod --port 21039\" |grep -v grep|awk '{print $1}' |xargs kill -2")
+  IO.poepn("rm -rf  ~/tmp/mongodb_test/tengine_production.*")
+end
 After do |s|
+  `touch ~/after_c2`
   IO.popen("ps -e |grep tengined|grep -v grep |awk '{print $1}' |xargs kill -9")
   IO.popen("ps -e |grep \"mongod --port 21039\" |grep -v grep|awk '{print $1}' |xargs kill -2")
 end
