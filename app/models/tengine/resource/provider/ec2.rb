f8859402c804cef9cc12a10e8e2cc736a10b87e3
秋間 武志 <akima@akima-no-MacBook-Air.local>
2011-09-27 00:47:47 +0900

    [tengine_resource.gemのドラフト設計・実装] Tengine::Resource::Provider::Ec2#update_physical_serversを実装しました

--- app/models/tengine/resource/provider/ec2.rb
+++ app/models/tengine/resource/provider/ec2.rb
@@ -3,17 +3,26 @@ class Tengine::Resource::Provider::Ec2 < Tengine::Resource::Provider
   validates_presence_of :credential
 
   def update_physical_servers
+    found_ids = []
     credential.connect do |conn|
-       # ec2.describe_availability_zones  #=> [{:region_name=>"us-east-1",
-       #                                        :zone_name=>"us-east-1a",
-       #                                        :zone_state=>"available"}, ... ]
+      # ec2.describe_availability_zones  #=> [{:region_name=>"us-east-1",
+      #                                        :zone_name=>"us-east-1a",
+      #                                        :zone_state=>"available"}, ... ]
+      # http://docs.amazonwebservices.com/AWSEC2/latest/APIReference/index.html?ApiReference-query-DescribeAvailabilityZones.html
       conn.describe_availability_zones.each do |hash|
         name = hash[:zone_name]
-        self.physical_servers.create!(:name => name, :provided_name => name, :status => hash[:zone_state])
-        
+        server = self.physical_servers.where(:provided_name => name).first
+        if server
+          server.update_attributes(:status => hash[:zone_state])
+        else
+          server = self.physical_servers.create!(:provided_name => name,
+            :name => name, :status => hash[:zone_state])
+        end
+        found_ids << server.id
       end
     end
-    
+    self.physical_servers.not_in(:_id => found_ids).each do |server|
+      server.update_attributes(:status => "not_found")
+    end
   end
-  
 end
