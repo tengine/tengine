9747fd0596d502af2e9fcd265c4e16189c38393d
g-morita <g-morita@ec-one.com>
2011-09-26 11:28:12 +0900

    イベント一覧の通知レベルの表示を修正した際、specの修正が漏れていたので対応しました。

--- spec/views/tengine/core/events/index.html.erb_spec.rb
+++ spec/views/tengine/core/events/index.html.erb_spec.rb
@@ -34,7 +34,7 @@ describe "tengine/core/events/index.html.erb" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Source Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => "debug", :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => false.to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
