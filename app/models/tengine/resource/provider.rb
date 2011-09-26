413cdbab5411cc992b647a6bd3a568a1403456f6
秋間 武志 <akima@akima-no-MacBook-Air.local>
2011-09-27 00:58:35 +0900

    [tengine_resource.gemのドラフト設計・実装] Tengine::Resource::Provider::Ec2#update_physical_serversの処理のうち、抽象化できる部分をTengine::Resource::Providerに移動しました

--- app/models/tengine/resource/provider.rb
+++ app/models/tengine/resource/provider.rb
@@ -15,4 +15,23 @@ class Tengine::Resource::Provider
   def update_virtual_servers       ; raise NotImplementedError end
   def update_virtual_server_imagess; raise NotImplementedError end
 
+  private
+  def update_physical_servers_by(hashs)
+    found_ids = []
+    hashs.each do |hash|
+      server = self.physical_servers.where(:provided_name => hash[:provided_name]).first
+      if server
+        server.update_attributes(:status => hash[:status])
+      else
+        server = self.physical_servers.create!(
+          :provided_name => hash[:provided_name],
+          :name => hash[:name],
+          :status => hash[:status])
+      end
+      found_ids << server.id
+    end
+    self.physical_servers.not_in(:_id => found_ids).each do |server|
+      server.update_attributes(:status => "not_found")
+    end
+  end
 end
