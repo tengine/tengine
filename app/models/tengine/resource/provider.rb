0d1aa4ca4b917292e62948d9ee93f26d451962b2
t-akima <akima@nautilus-technologies.com>
2011-09-26 16:36:09 +0900

    [tengine_resource.gemのドラフト設計・実装] Tengine::Resource::VirtualServerImageとTengine::Resource::Providerの関連を設定しました

--- app/models/tengine/resource/provider.rb
+++ app/models/tengine/resource/provider.rb
@@ -4,8 +4,9 @@ class Tengine::Resource::Provider
   field :description, :type => String
 
   with_options(:inverse_of => :provider, :dependent => :destroy) do |c|
-    c.has_many :virtual_servers , :class_name => "Tengine::Resource::VirtualServer"
-    c.has_many :physical_servers, :class_name => "Tengine::Resource::PhysicalServer"
+    c.has_many :physical_servers       , :class_name => "Tengine::Resource::PhysicalServer"
+    c.has_many :virtual_servers        , :class_name => "Tengine::Resource::VirtualServer"
+    c.has_many :virtual_server_imagess , :class_name => "Tengine::Resource::VirtualServerImage"
   end
 
 end
--- app/models/tengine/resource/virtual_server_image.rb
+++ app/models/tengine/resource/virtual_server_image.rb
@@ -3,5 +3,6 @@ class Tengine::Resource::VirtualServerImage
   field :name, :type => String
   field :description, :type => String
   field :provided_name, :type => String
-  referenced_in :provider, :inverse_of => :virtual_server_images, :index => true
+  belongs_to :provider, :inverse_of => :virtual_server_images, :index => true,
+    :class_name => "Tengine::Resource::Provider"
 end
