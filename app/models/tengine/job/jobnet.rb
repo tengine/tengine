808a403585ae7b92a7cfa82524010bbaa136131e
秋間 武志 <akima@akima-no-MacBook-Air.local>
2011-09-28 01:21:08 +0900

    [tengine_job.gemのドラフト設計・実装] finallyのjobnetを定義しました

--- app/models/tengine/job/jobnet.rb
+++ app/models/tengine/job/jobnet.rb
@@ -1,10 +1,17 @@
 class Tengine::Job::Jobnet < Tengine::Job::Job
+  include SelectableAttr::Base
+
   include Tengine::Job::Root
   field :description, :type => String
-  field :jobnet_type_cd, :type => Integer
+  field :jobnet_type_cd, :type => Integer, :default => 1
+
+  selectable_attr :jobnet_type_cd do
+    entry 1, :normal , "normal"
+    entry 2, :finally, "finally"
+    # entry 3, :recover, "recover"
+  end
 
   embeds_many :edges, :class_name => "Tengine::Job::Edge", :inverse_of => :owner
-
 
   def build_sequencial_edges
     self.edges.clear
@@ -16,12 +23,18 @@ class Tengine::Job::Jobnet < Tengine::Job::Job
     end
     current = nil
     self.children.each do |child|
+      child.build_sequencial_edges if child.respond_to?(:build_sequencial_edges)
+      next if child.is_a?(Tengine::Job::Jobnet) && (child.jobnet_type_key != :normal)
       if current
         self.edges.new(:origin_id => current.id, :destination_id =>child.id)
       end
       current = child
-      child.build_sequencial_edges if child.respond_to?(:build_sequencial_edges)
     end
   end
+
+  def finally_jobnet
+    self.children.detect{|c| c.is_a?(Tengine::Job::Jobnet) && (c.jobnet_type_key == :finally)}
+  end
+
 
 end
--- spec/models/tengine/job/jobnet_template_spec.rb
+++ spec/models/tengine/job/jobnet_template_spec.rb
@@ -67,6 +67,71 @@ describe Tengine::Job::JobnetTemplate do
         ]
       end
     end
+
+    context "finally入りの場合" do
+      #                               |--- job111
+      #             |--- jobnet11<>---|
+      #             |                 |--- job112
+      #             |
+      # jobnet1<>---|
+      #             |                 |--- job121
+      #             |--- finally<>---|
+      #                               |--- job122
+      #
+      # 以下のように実行されるように
+      # jobnet1
+      # jobnet1/jobnet11
+      # jobnet1/jobnet11/job111
+      # jobnet1/jobnet11/job112
+      # jobnet1/finally
+      # jobnet1/finally/job121
+      # jobnet1/finally/job122
+      it "レシーバ以下のジョブネットに対してシーケンシャルにedgesを構築する" do
+        jobnet1 = Tengine::Job::JobnetTemplate.new(:name => "jobnet1")
+        jobnet1 .children << jobnet11 = Tengine::Job::JobnetTemplate.new(:name => "jobnet11")
+        jobnet11.children << job111   = Tengine::Job::ScriptTemplate.new(:name => "job111", :script => "job111.sh")
+        jobnet11.children << job112   = Tengine::Job::ScriptTemplate.new(:name => "job112", :script => "job112.sh")
+        jobnet1 .children << finally = Tengine::Job::JobnetTemplate.new(:name => "finally", :jobnet_type_key => :finally)
+        finally.children << job121   = Tengine::Job::ScriptTemplate.new(:name => "job121", :script => "job121.sh")
+        finally.children << job122   = Tengine::Job::ScriptTemplate.new(:name => "job122", :script => "job122.sh")
+
+        jobnet1.build_sequencial_edges
+        jobnet1.children.map(&:class).should == [
+          Tengine::Job::Start,
+          Tengine::Job::JobnetTemplate,
+          Tengine::Job::JobnetTemplate, # :finallyはEndの前にあります。
+          Tengine::Job::End
+        ]
+        jobnet1.finally_jobnet.should == finally
+
+        jobnet1.edges.map{|edge| [edge.origin_id, edge.destination_id]}.should == [
+          [jobnet1.children.first.id, jobnet11.id],
+          [jobnet11.id, jobnet1.children.last.id],
+        ]
+        jobnet11.children.map(&:class).should == [
+          Tengine::Job::Start,
+          Tengine::Job::ScriptTemplate,
+          Tengine::Job::ScriptTemplate,
+          Tengine::Job::End
+        ]
+        jobnet11.edges.map{|edge| [edge.origin_id, edge.destination_id]}.should == [
+          [jobnet11.children.first.id, job111.id],
+          [job111.id, job112.id],
+          [job112.id, jobnet11.children.last.id],
+        ]
+        finally.children.map(&:class).should == [
+          Tengine::Job::Start,
+          Tengine::Job::ScriptTemplate,
+          Tengine::Job::ScriptTemplate,
+          Tengine::Job::End
+        ]
+        finally.edges.map{|edge| [edge.origin_id, edge.destination_id]}.should == [
+          [finally.children.first.id, job121.id],
+          [job121.id, job122.id],
+          [job122.id, finally.children.last.id],
+        ]
+      end
+    end
   end
 
 end
