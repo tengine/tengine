7028e9e9d7a7d0acd77c2fa2406f69f0f6db8641
t-akima <akima@nautilus-technologies.com>
2011-09-27 09:41:41 +0900

    [tengine_resource.gemのドラフト設計・実装] 仮想サーバのテストを追加する前に、物理サーバの更新のテストをコンテキストを別にしました

--- spec/models/tengine/resource/provider/ec2_spec.rb
+++ spec/models/tengine/resource/provider/ec2_spec.rb
@@ -33,88 +33,22 @@ describe Tengine::Resource::Provider::Ec2 do
       Tengine::Resource::Provider::Ec2.create!(:name => "ec2 us-west-1", :credential => @credential)
     end
 
-    before do
-      Tengine::Resource::PhysicalServer.delete_all
-      # spec/support/ec2.rb を参照してください
-      RightAws::Ec2.should_receive(:new).
-        with('ACCESS_KEY1', "SECRET_ACCESS_KEY1", :region => "us-west-1", :logger => Rails.logger).
-        and_return(setup_ec2_stub)
-    end
+    context "物理サーバ" do
 
-    shared_examples_for "取得した内容が反映される" do
-      it do
-        servers = subject.physical_servers.order(:provided_name)
-        west_1a = servers.first
-        west_1a.provider.should == subject
-        west_1a.name.should == "us-west-1a"
-        west_1a.provided_name.should == "us-west-1a"
-        west_1a.status.should == "available"
-        west_1b = servers.last
-        west_1b.provider.should == subject
-        west_1b.name.should == "us-west-1b"
-        west_1b.provided_name.should == "us-west-1b"
-        west_1b.status.should == "available"
-      end
-    end
-
-    context "最初の実行時には物理サーバを登録する" do
       before do
-        subject.physical_servers.count.should == 0
-        subject.update_physical_servers
-        subject.physical_servers.count.should == 2
-      end
-      it_behaves_like "取得した内容が反映される"
-    end
-
-    context "すでに1台登録されている場合" do
-      before do
-        subject.physical_servers.create(:name => "us-west-1a", :provided_name => "us-west-1a", :status => "available")
-        subject.physical_servers.count.should == 1
-        subject.update_physical_servers
-        subject.physical_servers.count.should == 2
-      end
-      it_behaves_like "取得した内容が反映される"
-    end
-
-    context "すでに2台登録されている場合" do
-      context "何も変わらない場合" do
-        before do
-          subject.physical_servers.create(:name => "us-west-1a", :provided_name => "us-west-1a", :status => "available")
-          subject.physical_servers.create(:name => "us-west-1b", :provided_name => "us-west-1b", :status => "available")
-          subject.physical_servers.count.should == 2
-          subject.update_physical_servers
-          subject.physical_servers.count.should == 2
-        end
-        it_behaves_like "取得した内容が反映される"
+        Tengine::Resource::PhysicalServer.delete_all
+        # spec/support/ec2.rb を参照してください
+        RightAws::Ec2.should_receive(:new).
+          with('ACCESS_KEY1', "SECRET_ACCESS_KEY1", :region => "us-west-1", :logger => Rails.logger).
+          and_return(setup_ec2_stub)
       end
 
-      context "状態が変わった場合" do
-        before do
-          subject.physical_servers.create(:name => "us-west-1a", :provided_name => "us-west-1a", :status => "available")
-          subject.physical_servers.create(:name => "us-west-1b", :provided_name => "us-west-1b", :status => "down")
-          # こんな定義はないはずですが、そもそも定義がないので。
-          # http://docs.amazonwebservices.com/AWSEC2/latest/APIReference/index.html?ApiReference-query-DescribeAvailabilityZones.html
-          subject.physical_servers.count.should == 2
-          subject.update_physical_servers
-          subject.physical_servers.count.should == 2
-        end
-        it_behaves_like "取得した内容が反映される"
-      end
-
-      context "名前が管理画面で変更された場合" do
-        before do
-          subject.physical_servers.create(:name => "foo", :provided_name => "us-west-1a", :status => "available")
-          subject.physical_servers.create(:name => "us-west-1b", :provided_name => "us-west-1b", :status => "available")
-          subject.physical_servers.count.should == 2
-          subject.update_physical_servers
-          subject.physical_servers.count.should == 2
-        end
-
+      shared_examples_for "取得した内容が反映される" do
         it do
           servers = subject.physical_servers.order(:provided_name)
           west_1a = servers.first
           west_1a.provider.should == subject
-          west_1a.name.should == "foo"
+          west_1a.name.should == "us-west-1a"
           west_1a.provided_name.should == "us-west-1a"
           west_1a.status.should == "available"
           west_1b = servers.last
@@ -124,36 +58,106 @@ describe Tengine::Resource::Provider::Ec2 do
           west_1b.status.should == "available"
         end
       end
+
+      context "最初の実行時には物理サーバを登録する" do
+        before do
+          subject.physical_servers.count.should == 0
+          subject.update_physical_servers
+          subject.physical_servers.count.should == 2
+        end
+        it_behaves_like "取得した内容が反映される"
+      end
+
+      context "すでに1台登録されている場合" do
+        before do
+          subject.physical_servers.create(:name => "us-west-1a", :provided_name => "us-west-1a", :status => "available")
+          subject.physical_servers.count.should == 1
+          subject.update_physical_servers
+          subject.physical_servers.count.should == 2
+        end
+        it_behaves_like "取得した内容が反映される"
+      end
+
+      context "すでに2台登録されている場合" do
+        context "何も変わらない場合" do
+          before do
+            subject.physical_servers.create(:name => "us-west-1a", :provided_name => "us-west-1a", :status => "available")
+            subject.physical_servers.create(:name => "us-west-1b", :provided_name => "us-west-1b", :status => "available")
+            subject.physical_servers.count.should == 2
+            subject.update_physical_servers
+            subject.physical_servers.count.should == 2
+          end
+          it_behaves_like "取得した内容が反映される"
+        end
+
+        context "状態が変わった場合" do
+          before do
+            subject.physical_servers.create(:name => "us-west-1a", :provided_name => "us-west-1a", :status => "available")
+            subject.physical_servers.create(:name => "us-west-1b", :provided_name => "us-west-1b", :status => "down")
+            # こんな定義はないはずですが、そもそも定義がないので。
+            # http://docs.amazonwebservices.com/AWSEC2/latest/APIReference/index.html?ApiReference-query-DescribeAvailabilityZones.html
+            subject.physical_servers.count.should == 2
+            subject.update_physical_servers
+            subject.physical_servers.count.should == 2
+          end
+          it_behaves_like "取得した内容が反映される"
+        end
+
+        context "名前が管理画面で変更された場合" do
+          before do
+            subject.physical_servers.create(:name => "foo", :provided_name => "us-west-1a", :status => "available")
+            subject.physical_servers.create(:name => "us-west-1b", :provided_name => "us-west-1b", :status => "available")
+            subject.physical_servers.count.should == 2
+            subject.update_physical_servers
+            subject.physical_servers.count.should == 2
+          end
+
+          it do
+            servers = subject.physical_servers.order(:provided_name)
+            west_1a = servers.first
+            west_1a.provider.should == subject
+            west_1a.name.should == "foo"
+            west_1a.provided_name.should == "us-west-1a"
+            west_1a.status.should == "available"
+            west_1b = servers.last
+            west_1b.provider.should == subject
+            west_1b.name.should == "us-west-1b"
+            west_1b.provided_name.should == "us-west-1b"
+            west_1b.status.should == "available"
+          end
+        end
+      end
+
+      context "すでに3台登録されている場合" do
+        before do
+          subject.physical_servers.create(:name => "us-west-1a", :provided_name => "us-west-1a", :status => "available")
+          subject.physical_servers.create(:name => "us-west-1b", :provided_name => "us-west-1b", :status => "available")
+          subject.physical_servers.create(:name => "us-west-1c", :provided_name => "us-west-1c", :status => "available")
+          subject.physical_servers.count.should == 3
+          subject.update_physical_servers
+          subject.physical_servers.count.should == 3
+        end
+        it "物理サーバが減るということは一大事なので、自動でデータを削除するのではなく、見つからなかったということにする" do
+          servers = subject.physical_servers.order(:provided_name)
+          west_1a = servers.first
+          west_1a.provider.should == subject
+          west_1a.name.should == "us-west-1a"
+          west_1a.provided_name.should == "us-west-1a"
+          west_1a.status.should == "available"
+          west_1b = servers[1]
+          west_1b.provider.should == subject
+          west_1b.name.should == "us-west-1b"
+          west_1b.provided_name.should == "us-west-1b"
+          west_1b.status.should == "available"
+          west_1c = servers[2]
+          west_1c.provider.should == subject
+          west_1c.name.should == "us-west-1c"
+          west_1c.provided_name.should == "us-west-1c"
+          west_1c.status.should == "not_found"
+        end
+      end
     end
 
-    context "すでに3台登録されている場合" do
-      before do
-        subject.physical_servers.create(:name => "us-west-1a", :provided_name => "us-west-1a", :status => "available")
-        subject.physical_servers.create(:name => "us-west-1b", :provided_name => "us-west-1b", :status => "available")
-        subject.physical_servers.create(:name => "us-west-1c", :provided_name => "us-west-1c", :status => "available")
-        subject.physical_servers.count.should == 3
-        subject.update_physical_servers
-        subject.physical_servers.count.should == 3
-      end
-      it "物理サーバが減るということは一大事なので、自動でデータを削除するのではなく、見つからなかったということにする" do
-        servers = subject.physical_servers.order(:provided_name)
-        west_1a = servers.first
-        west_1a.provider.should == subject
-        west_1a.name.should == "us-west-1a"
-        west_1a.provided_name.should == "us-west-1a"
-        west_1a.status.should == "available"
-        west_1b = servers[1]
-        west_1b.provider.should == subject
-        west_1b.name.should == "us-west-1b"
-        west_1b.provided_name.should == "us-west-1b"
-        west_1b.status.should == "available"
-        west_1c = servers[2]
-        west_1c.provider.should == subject
-        west_1c.name.should == "us-west-1c"
-        west_1c.provided_name.should == "us-west-1c"
-        west_1c.status.should == "not_found"
-      end
-    end
   end
 
 end
