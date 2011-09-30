65d79b0c6ff1d8635ccaab51b6e7b7cdfe981c01
akima <akima@nautilus-technologies.com>
2011-09-30 16:43:33 +0900

    [tengine_job.gemのドラフト設計・実装] Tengine::Job::Categoryがツリー構造を持つように関連を設定しました

--- app/models/tengine/job/category.rb
+++ app/models/tengine/job/category.rb
@@ -2,5 +2,8 @@ class Tengine::Job::Category
   include Mongoid::Document
   field :name, :type => String
   field :caption, :type => String
-  referenced_in :parent, :inverse_of => :categories, :index => true
+  with_options(:class_name => "Tengine::Job::Category") do |c|
+    c.belongs_to :parent, :inverse_of => :children, :index => true
+    c.has_many   :children, :inverse_of => :parent
+  end
 end
