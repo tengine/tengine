4b1631ad0580d09dc00f15564eaafcc3ef4b4ac4
Taku Yamada <t-yamada@nautilus-technologies.com>
2011-10-03 19:51:54 +0900

    [{アプリケーション開発者がジョブネット定義を試してみる}] DSLを追加しました

---
+++ usecases/job/dsl/1038_not_refrenced_job_in_jobnet.rb
@@ -0,0 +1,6 @@
+jobnet("jobnet1038", :instance_name => "i-11111111", :credential_name => "goku-ssh-pk1") do
+  boot_jobs("job1")
+  job("job1", "echo 'job1'", :to => "job2")
+  job("job2", "echo 'job2'")
+  job("job3", "echo 'job3'")
+end
