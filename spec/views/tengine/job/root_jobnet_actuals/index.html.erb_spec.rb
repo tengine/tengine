eafb348468b21d77354dcc728e016cb4b1307166
ç§é æ­¦å¿ <akima@akima-no-MacBook-Air.local>
2011-10-12 13:17:48 +0900

    [ジョブ制御ドライバの実装(正常系)] rake specをパスするようにビューのspecを修正しました

--- spec/views/tengine/job/root_jobnet_actuals/index.html.erb_spec.rb
+++ spec/views/tengine/job/root_jobnet_actuals/index.html.erb_spec.rb
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "tengine/job/root_jobnet_actuals/index.html.erb" do
   before(:each) do
+    stub_category = stub_model(Tengine::Job::Category, :caption => "カテゴリ1")
+    stub_template = stub_model(Tengine::Job::RootJobnetTemplate, :name => "root_jobnet1")
     mock_pagination(assign(:root_jobnet_actuals, [
       stub_model(Tengine::Job::RootJobnetActual,
         :name => "Name",
@@ -11,15 +14,15 @@ describe "tengine/job/root_jobnet_actuals/index.html.erb" do
         :killing_signal_interval => 1,
         :description => "Description",
         :script => "Script",
-        :jobnet_type_cd => 1,
+        :jobnet_type_cd => 2,
         :executing_pid => "Executing Pid",
         :exit_status => "Exit Status",
         :was_expansion => false,
-        :phase_cd => 1,
+        :phase_cd => 3,
         :stop_reason => "Stop Reason",
-        :category => nil,
-        :lock_version => 1,
-        :template => nil
+        :category => stub_category,
+        :lock_version => 4,
+        :template => stub_template
       ),
       stub_model(Tengine::Job::RootJobnetActual,
         :name => "Name",
@@ -29,15 +32,15 @@ describe "tengine/job/root_jobnet_actuals/index.html.erb" do
         :killing_signal_interval => 1,
         :description => "Description",
         :script => "Script",
-        :jobnet_type_cd => 1,
+        :jobnet_type_cd => 2,
         :executing_pid => "Executing Pid",
         :exit_status => "Exit Status",
         :was_expansion => false,
-        :phase_cd => 1,
+        :phase_cd => 3,
         :stop_reason => "Stop Reason",
-        :category => nil,
-        :lock_version => 1,
-        :template => nil
+        :category => stub_category,
+        :lock_version => 4,
+        :template => stub_template
       )
     ]))
   end
@@ -52,14 +55,14 @@ describe "tengine/job/root_jobnet_actuals/index.html.erb" do
     assert_select "tr>td", :text => 1.to_s, :count => 2
     assert_select "tr>td", :text => "Description".to_s, :count => 2
     assert_select "tr>td", :text => "Script".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
     assert_select "tr>td", :text => "Executing Pid".to_s, :count => 2
     assert_select "tr>td", :text => "Exit Status".to_s, :count => 2
     assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => 3.to_s, :count => 2
     assert_select "tr>td", :text => "Stop Reason".to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => "カテゴリ1", :count => 2
+    assert_select "tr>td", :text => 4.to_s, :count => 2
+    assert_select "tr>td", :text => "root_jobnet1", :count => 2
   end
 end
