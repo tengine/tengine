2ab618d44f82c123ccb8fcbfd895d9d324cf9d40
Masamitsu Ogura <masamitsu.ogura@gmail.com>
2011-10-13 16:54:37 +0900

    [テンプレートジョブ一覧画面の実装] テンプレートジョブ一覧の機能を載せる画面のベースを作りました。

--- spec/views/tengine/job/root_jobnet_templates/index.html.erb_spec.rb
+++ spec/views/tengine/job/root_jobnet_templates/index.html.erb_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "tengine/job/root_jobnet_templates/index.html.erb" do
@@ -5,6 +6,7 @@ describe "tengine/job/root_jobnet_templates/index.html.erb" do
     category = stub_model(Tengine::Job::Category, :to_s => "category")
     mock_pagination(assign(:root_jobnet_templates, [
       stub_model(Tengine::Job::RootJobnetTemplate,
+        :id => BSON::ObjectId("4e955633c3406b3a9f000001"),
         :name => "Name",
         :server_name => "Server Name",
         :credential_name => "Credential Name",
@@ -20,6 +22,7 @@ describe "tengine/job/root_jobnet_templates/index.html.erb" do
         :dsl_version => "Dsl Version"
       ),
       stub_model(Tengine::Job::RootJobnetTemplate,
+        :id => BSON::ObjectId("4e955633c3406b3a9f000001"),
         :name => "Name",
         :server_name => "Server Name",
         :credential_name => "Credential Name",
@@ -40,18 +43,9 @@ describe "tengine/job/root_jobnet_templates/index.html.erb" do
   it "renders a list of tengine_job_root_jobnet_templates" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => BSON::ObjectId("4e955633c3406b3a9f000001").to_s, :count => 2
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Server Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Credential Name".to_s, :count => 2
-    assert_select "tr>td", :text => "abc,123", :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
     assert_select "tr>td", :text => "Description".to_s, :count => 2
-    assert_select "tr>td", :text => "Script".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => "category", :count => 2
-    assert_select "tr>td", :text => 3.to_s, :count => 2
-    assert_select "tr>td", :text => "Dsl Filepath".to_s, :count => 2
-    assert_select "tr>td", :text => 4.to_s, :count => 2
-    assert_select "tr>td", :text => "Dsl Version".to_s, :count => 2
   end
+
 end
