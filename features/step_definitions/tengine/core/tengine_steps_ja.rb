8a7c5bdd53eece5c29a710f6510921c6c0418111
g-morita <g-morita@nautilus-technologies.com>
2011-10-20 16:40:45 +0900

    [CI環境を整備する] pid,status,activationファイルを削除するステップ定義を追加しました。 "Tengineコアプロセス"が停止している でTengineコアプロセスを全てkillするようにしました。

--- features/step_definitions/tengine/core/tengine_steps_ja.rb
+++ features/step_definitions/tengine/core/tengine_steps_ja.rb
@@ -89,19 +89,36 @@ end
       raise "RabbitMQの停止に失敗しました" unless system('rabbitmqctl stop')
     end
   elsif name == "Tengineコアプロセス"
-    # Tengineコアのpidファイル => tmp/tengine_pids/tengine.[0からの連番].[pid]
-    # 例：tmp/tengine_pids/tengine.0.3948
-    # ファイルの中はpidが記述されている
-    pids = IO.popen("cat tmp/tengine_pids/tengine.*").to_a
-    pids.each do |pid|
-      if system('ps -eo pid #{pid}}')
-        raise "Tengineコアの停止に失敗しました" unless system("kill -KILL #{pid}")
-      end
-    end
+#    # Tengineコアのpidファイル => tmp/tengine_pids/tengine.[0からの連番].[pid]
+#    # 例：tmp/tengine_pids/tengine.0.3948
+#    # ファイルの中はpidが記述されている
+#    pids = IO.popen("cat tmp/tengine_pids/tengine.*").to_a
+#    pids.each do |pid|
+#      if system('ps -eo pid #{pid}}')
+#        raise "Tengineコアの停止に失敗しました" unless system("kill -KILL #{pid}")
+#      end
+#    end
+
+    # 起動しているTengineコアプロセスを全てkillする
+    tengine_core_process_kill_all
   elsif name == "Tengineコンソールプロセス"
     if system('ps -eo pid | grep `cat tmp/pids/server.pid`')
       raise "Tengineコアの停止に失敗しました" unless system('kill -KILL `cat tmp/pids/server.pid`')
     end
+  end
+end
+
+# ファイルは ./tmp/tengined_pids, ./tmp/tengined_status, ./tmp/tengined_activations ディレクトリ以下のファイルを想定
+# 明示的にパスを指定して起動されたTengineコアプロセスのファイルについては対象外です。
+前提 /^"([^"]*)"の"([^"]*)ファイル"が存在しない$/ do |name, file_name|
+  if file_name == "pid"
+    system('rm -fr ./tmp/tengined_pids/*')
+  elsif file_name == "status"
+    system('rm -fr ./tmp/tengined_status/*')
+  elsif file_name == "activation"
+    system('rm -fr ./tmp/tengined_activations/*')
+  else
+    raise "#{file_name}ファイルは、サポート外のファイルです。"
   end
 end
 
@@ -609,10 +626,13 @@ def view_time_format
 end
 
 # 起動した順番のpidの配列を返す
+# 注意：このメソッドはTengineコアプロセスがデーモンモードで起動していることを想定しています。
+#      デーモンモードと、そうでない場合では、 ps が出力する COMMAND の値に差異があるためです。
+#     
 def tengine_core_process_pids(status)
   pids = []
   lines = []
-  # 出力結果の例：
+  # 出力結果の例：（デーモンモードで起動した場合のみ）
   # tengined.0:58812
   # tengined.1:58834
   command = "ps aux | grep tengined | awk '{print $11\":\"$2}'"
@@ -628,8 +648,8 @@ def tengine_core_process_pids(status)
   end
  
  puts lines
+# => ["tengined.0:58812", "tengined.1:58834"]
 
-#  lines = ["tengined.0:58812", "tengined.1:58834"]
   lines.sort! do |a, b|
      a.split(':')[0] <=> b.split(':')[0]
   end
@@ -657,6 +677,18 @@ def tengine_core_process_start_time(pid)
   end
   puts "tengine_core_process_start_time start_time => #{start_time}"
   Time.parse(start_time)
+end
+
+# 全てのTengineコアプロセスをkillします
+def tengine_core_process_kill_all
+  pids = tengine_core_process_running_pids
+  pids.each do |pid|
+    if system('ps -eo pid #{pid}}')
+      command = "kill -KILL #{pid}"
+      puts "tengine_core_process_kill_all command => #{command}"
+      raise "Tengineコアの停止に失敗しました" unless system(command)
+    end
+  end
 end
 
 def time_out(time, &block)
