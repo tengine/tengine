d851f22c772a8579ee23a99a1a00007846f4f439
akima <akima@nautilus-technologies.com>
2011-10-25 11:32:29 +0900

    [ジョブの強制停止の実装] endが一つ多いことに気づいちゃったので消しておきました

--- usecases/job/dsl/1042_duplicated_jobname_on_diff_layer.rb
+++ usecases/job/dsl/1042_duplicated_jobname_on_diff_layer.rb
@@ -4,6 +4,5 @@ jobnet("jobnet1042", :instance_name => "i-11111111", :credential_name => "goku-s
   jobnet("jobnet1042_2") do
     auto_sequence
     job("job1", "echo 'job1-2'")
-    end
   end
 end
