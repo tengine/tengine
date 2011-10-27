51f67c705718348f4370627f65813ecc9feaa0c2
Taku Yamada <t-yamada@nautilus-technologies.com>
2011-10-27 13:54:03 +0900

    [ジョブの基本機能[ユースケース]] ジョブネット名が間違っていたので修正しました

--- usecases/job/dsl/1044_not_registered_credential_name.rb
+++ usecases/job/dsl/1044_not_registered_credential_name.rb
@@ -1,7 +1,7 @@
 
 require 'tengine_job'
 
-jobnet("jobnet1001", :instance_name => "test_server1", :credential_name => "not_registered") do
+jobnet("jobnet1044", :instance_name => "test_server1", :credential_name => "not_registered") do
   boot_jobs("job1")
   job("job1", "echo 'job1'")
 end
