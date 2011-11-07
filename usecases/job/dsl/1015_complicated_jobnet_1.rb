719cc7460774a2dd306f17de1b2827333aaca667
Taku Yamada <t-yamada@nautilus-technologies.com>
2011-11-07 18:49:39 +0900

    [tengine_job[ユースケース]] DSLをtengine_job_test.shを使用するかたちに変更しました

--- usecases/job/dsl/1015_complicated_jobnet_1.rb
+++ usecases/job/dsl/1015_complicated_jobnet_1.rb
@@ -8,48 +8,36 @@ jobnet('complicated_jobnet', '複雑なジョブネット', :vm_instance_name =>
 boot_jobs("i_jobnet1-1", "i_jobnet1-2", "i_jobnet1-3", "i_jobnet2-1", "i_jobnet2-2")  
   jobnet('i_jobnet1-1', :vm_instance_name => "test_server1",:credential_name => "test_credential1",:to => "i_jobnet3") do
       auto_sequence
-      job("i_jobnet1-1-0", "echo 'jobnet1-1 START'")
-      job("i_jobnet1-1-1", "sleep 5")
-      job("i_jobnet1-1-2", "echo 'jobnet1-1 END'")
+      job("i_jobnet1-1", "$HOME/tengine_job_test.sh 5 jobnet1-1")
   end
   jobnet('i_jobnet1-2',  :vm_instance_name => "test_server1",:credential_name => "test_credential1",:to => "i_jobnet3") do
       auto_sequence
-      job("i_jobnet1-2-0", "echo 'jobnet1-2 START'")
-      job("i_jobnet1-2-1", "sleep 10")
-      job("i_jobnet1-2-2", "echo 'jobnet1-2 END'")
+      job("i_jobnet1-2", "$HOME/tengine_job_test.sh 10 jobnet1-2")
   end
   jobnet('i_jobnet1-3', :vm_instance_name => "test_server1",:credential_name => "test_credential1",:to => "i_jobnet3") do
       boot_jobs("i_job1-1","i_job1-2","i_job1-3","i_job2-1","i_job2-2")
-      job("i_job1-1", "sleep 1",:to => "i_job3")
-      job("i_job1-2", "sleep 1",:to => "i_job3")
-      job("i_job1-3", "sleep 1",:to => "i_job3")
+      job("i_job1-1", "$HOME/tengine_job_test.sh 1 job1-1",:to => "i_job3")
+      job("i_job1-2", "$HOME/tengine_job_test.sh 1 job1-2",:to => "i_job3")
+      job("i_job1-3", "$HOME/tengine_job_test.sh 1 job1-3",:to => "i_job3")
       job("i_job2-1", "sleeeeeep 1",:to => "i_job2-0")
-      job("i_job2-2", "sleep 1",:to => "i_job2-0")
-      job("i_job2-0", "sleep 1",:to => "i_job3")
-      job("i_job3", "sleep 1")
+      job("i_job2-2", "$HOME/tengine_job_test.sh 1 job2-2",:to => "i_job2-0")
+      job("i_job2-0", "$HOME/tengine_job_test.sh 1 job2-0",:to => "i_job3")
+      job("i_job3", "$HOME/tengine_job_test.sh 1 job3")
   end
   jobnet('i_jobnet2-1',  :vm_instance_name => "test_server1",:credential_name => "test_credential1",:to => "i_jobnet2-0") do   
       auto_sequence
-      job("i_jobnet2-1-0", "echo 'jobnet2-1 START'")
-      job("i_jobnet2-1-1", "sleep 1")
-      job("i_jobnet2-1-2", "echo 'jobnet2-1 END'")
+      job("i_jobnet2-1", "$HOME/tengine_job_test.sh 1 jobnet2-1")
   end
   jobnet('i_jobnet2-2', :vm_instance_name => "test_server1",:credential_name => "test_credential1",:to => "i_jobnet2-0") do    
       auto_sequence
-      job("i_jobnet2-2-0", "echo 'jobnet2-2 START'")
-      job("i_jobnet2-2-1", "sleep 1")
-      job("i_jobnet2-2-2", "echo 'jobnet2-2 END'")
+      job("i_jobnet2-2", "$HOME/tengine_job_test.sh 1 jobnet2-2")
   end
   jobnet('i_jobnet2-0',  :vm_instance_name => "test_server1",:credential_name => "test_credential1",:to => "i_jobnet3") do
       auto_sequence
-      job("i_jobnet2-0-0", "echo 'jobnet2-0 START'")
-      job("i_jobnet2-0-1", "sleep 1")
-      job("i_jobnet2-0-2", "echo 'jobnet2-0 END'")
+      job("i_jobnet2-0", "$HOME/tengine_job_test.sh 1 jobnet2-0")
   end
   jobnet('i_jobnet3',  :vm_instance_name => "test_server1",:credential_name => "test_credential1") do
       auto_sequence
-      job("i_jobnet3-0", "echo 'jobnet3 START'")
-      job("i_jobnet3-1", "sleep 1")
-      job("i_jobnet3-2", "echo 'jobnet3 END'")
+      job("i_jobnet3", "$HOME/tengine_job_test.sh 1 jobnet3")
   end
 end
