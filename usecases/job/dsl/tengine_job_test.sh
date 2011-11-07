fe321f4d9d13f28f268c165c604cbe48a70458d0
Taku Yamada <t-yamada@nautilus-technologies.com>
2011-11-07 17:09:37 +0900

    [tengine_job[ユースケース]] tengine_job_test.shにプロセスIDを出力するコードを追加しました

--- usecases/job/dsl/tengine_job_test.sh
+++ usecases/job/dsl/tengine_job_test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 typeset -i time=$1
 export LOGFILE=tengine_job_test.log
+echo "`date` tengine_job_test PID:$$" >> $LOGFILE
 echo "`date` tengine_job_test $2 start" >> $LOGFILE
 sleep `expr 1 + $time`
 echo "`date` tengine_job_test $2 finish" >> $LOGFILE
