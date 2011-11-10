4f319523f1bfb196f653ec9bcba3ee0614befbba
NaCl ando <ando@netlab.jp>
2011-11-11 04:13:56 +0900

    [クレデンシャルの登録画面を作成する] 編集画面でにSSHパスワード認証からSSH公開鍵認証に変更したときにusernameに値が残る問題を修正しました。各画面のテストを追加しました。

--- spec/views/tengine/resource/credentials/index.html.erb_spec.rb
+++ spec/views/tengine/resource/credentials/index.html.erb_spec.rb
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-=begin
 require 'spec_helper'
 
 describe "tengine/resource/credentials/index.html.erb" do
   before(:each) do
-    mock_pagination(assign(:credentials, [
+    assign(:credentials, Kaminari.paginate_array([
       stub_model(Tengine::Resource::Credential,
         :id=>BSON::ObjectId("4eb6a247df46903c8600007b"),
         :name => "ssh_password",
@@ -33,7 +32,7 @@ describe "tengine/resource/credentials/index.html.erb" do
         :created_at => Time.now,
         :updated_at => Time.now
       )
-    ]))
+    ]).page(1))
   end
 
   it "renders a list of tengine_resource_credentials" do
@@ -71,8 +70,64 @@ describe "tengine/resource/credentials/index.html.erb" do
       :text => Tengine::Resource::Credential.human_attribute_name(:auth_type_cd))
   end
 
+  context "nameの昇順で一覧を表示しているとき" do
+    before(:each) do
+      @request.query_parameters[:sort] = {"name" => "asc"}
+      @request.params[:sort] = {"name" => "asc"}
+    end
 
+    it "nameのソートのリンクに降順のクエリーパラメータが付加されていてclassがascになっていること" do
+      render
 
+      href = tengine_resource_credentials_path(:sort=>{:name=>"desc"})
+      rendered.should have_xpath("//a[@class='asc'][@href='#{href}']",
+        :text => Tengine::Resource::Credential.human_attribute_name(:name))
+    end
 
+    it "descriptionのソートのリンクに昇順のクエリーパラメータが付加されていてclassが空文字列となっていること" do
+      render
 
-=end
+      href = tengine_resource_credentials_path(:sort=>{:description=>:asc})
+      rendered.should have_xpath("//a[@class=''][@href='#{href}']",
+        :text => Tengine::Resource::Credential.human_attribute_name(:description))
+    end
+  end
+
+  it "検索フォームに値が入っていないこと、セレクトボックスがすべてチェックされていること" do
+    render
+
+    assert_select "input[id='finder_name']", :text => "", :count => 1
+    assert_select "input[id='finder_description']", :text => "", :count => 1
+    assert_select "input[id='finder_auth_type_cd_01'][type = checkbox][value = 1]"
+    assert_select "input[id='finder_auth_type_cd_02'][type = checkbox][value = 1]"
+    assert_select "input[id='finder_auth_type_cd_03'][type = checkbox][value = 1]"
+    
+  end
+
+  it "検索ボタンが表示されていること" do
+    render
+
+    rendered.should have_button(I18n.t("views.links.search"))
+  end
+
+  it "リセットボタンが表示されていること" do
+    render
+
+    rendered.should have_xpath(%|//input[@type='reset'][@value='#{I18n.t("views.links.reset")}']|, :count => 1)
+  end
+
+  it "ソートのリンクが表示されていること" do
+    render
+
+    href = tengine_resource_credentials_path(:sort=>{:name=>"asc"})
+    rendered.should have_xpath("//a[@class=''][@href='#{href}']",
+      :text => Tengine::Resource::Credential.human_attribute_name(:name))
+    href = tengine_resource_credentials_path(:sort=>{:description=>"asc"})
+    rendered.should have_xpath("//a[@class=''][@href='#{href}']",
+      :text => Tengine::Resource::Credential.human_attribute_name(:description))
+    href = tengine_resource_credentials_path(:sort=>{:auth_type_cd=>"asc"})
+    rendered.should have_xpath("//a[@class=''][@href='#{href}']",
+      :text => Tengine::Resource::Credential.human_attribute_name(:auth_type_cd))
+  end
+
+end
