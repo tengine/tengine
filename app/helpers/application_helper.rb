373cfbb2dea701b5d7d5394bfeff0debd932d350
Masamitsu Ogura <masa@netlab.jp>
2011-11-16 15:42:53 +0900

    [Cacooの画面仕様変更の反映] 一覧の表のソートリンクのクラスを修正しました

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,15 +72,13 @@ module ApplicationHelper
   end
 
   ENABLE_ORDER = ["asc", "desc"].freeze
-  ASC_CLASS = "asc"
-  DESC_CLASS = "desc"
   NO_ORDER_CLASS = ""
 
   def sort_class(sym)
     return NO_ORDER_CLASS if (sort = request.query_parameters[:sort]).blank?
     return NO_ORDER_CLASS if (current_order = sort[sym].to_s).blank?
     return NO_ORDER_CLASS unless ENABLE_ORDER.include?(current_order)
-    return current_order
+    return "Sort#{current_order.camelcase}"
   end
 
   def sort_param(sym)
