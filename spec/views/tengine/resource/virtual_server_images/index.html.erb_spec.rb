8473b5c75d3890f1f75590c15c215f493f2b5602
NaCl ando <ando@netlab.jp>
2011-11-21 01:05:07 +0900

    [仮想サーバイメージ一覧画面を実装する]画面のテストを一部用意しました

--- spec/views/tengine/resource/virtual_server_images/index.html.erb_spec.rb
+++ spec/views/tengine/resource/virtual_server_images/index.html.erb_spec.rb
@@ -8,13 +8,13 @@ describe "tengine/resource/virtual_server_images/index.html.erb" do
         :name => "Name",
         :provided_id => "Provided Name",
         :provided_description => "Provided Description",
-        :description => "Description"
+        :description => "descriptions"
       ),
       stub_model(Tengine::Resource::VirtualServerImage,
         :name => "Name",
         :provided_id => "Provided Name",
         :provided_description => "Provided Description",
-        :description => "Description"
+        :description => "descriptions"
       )
     ]).page(1).per(5))
 
@@ -29,8 +29,8 @@ describe "tengine/resource/virtual_server_images/index.html.erb" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Provided Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Provided Description".to_s, :count => 2
-    assert_select "tr>td", :text => /Description/, :count => 2
+    assert_select "tr>td", :text => /Provided Description/, :count => 2
+    assert_select "tr>td", :text => /descriptions/, :count => 2
   end
 
   it "nameのソートのリンクに昇順のクエリーパラメータが付加されていてclassが空文字列となっていること" do
