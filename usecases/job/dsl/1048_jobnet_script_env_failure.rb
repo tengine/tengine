140f87213fdd969ffe9170c6ad07ccc13c0d791b
Yuuki Noguchi <otonakaoru@gmail.com>
2011-11-29 14:32:46 +0900

    [ジョブの基本機能[feature]]ジョブネット定義に構文ミスがあったため、修正しました。また、シナリオ名が分かりづらい為、修正しました。

--- usecases/job/dsl/1048_jobnet_script_env_failure.rb
+++ usecases/job/dsl/1048_jobnet_script_env_failure.rb
@@ -5,7 +5,7 @@ require 'tengine_job'
 jobnet("jobnet1048_failure", :instance_name => "test_server1", :credential_name => "test_credential1") do
   auto_sequence
   jobnet("jobnet1048_2") do
-    job("job1", "exit 1"
+    job("job1", "exit 1")
   end
   finally do
     job("jobnet1048_finally","$HOME/tengine_job_env_test.sh 0 jobnet1048_finally")
