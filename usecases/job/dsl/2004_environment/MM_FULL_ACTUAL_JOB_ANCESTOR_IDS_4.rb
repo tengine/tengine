333cc323ca0a9fa42e7e3762a8d2c774933d404b
Yuuki Noguchi <otonakaoru@gmail.com>
2011-12-19 22:47:53 +0900

    [2003_expansion/expansion_5.rbの意図をfeatureに記述する]不必要となったジョブネット定義を削除します

--- usecases/job/dsl/2004_environment/MM_FULL_ACTUAL_JOB_ANCESTOR_IDS_4.rb
+++
@@ -1,9 +0,0 @@
-
-require 'tengine_job'
-
-jobnet("expansion_jobnet_99_4_1", :vm_instance_name => "test_server1", :credential_name => "test_credential1") do
-  auto_sequence
-  expansion("child_root1")
-  expansion("child_root2")
-  job("job33", "~/echo_ancestor.sh")
-end
