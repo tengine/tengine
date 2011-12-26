ad9153890590bc05a12952c4b739259dc56c3520
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-12-26 14:04:31 +0900

    [tengine consoleのイベント発火画面で、なにもせず空欄のままで「登録する」をクリックしてしまうと、裏で動いているtenginedが落ちる(console側対応)]イベント発火画面からイベント発火を行う際にバリデーションチェックを行うようにしました。 バリデーションルールはTengine::Core::Eventを使用しています

--- app/controllers/tengine/core/event_ignitions_controller.rb
+++ app/controllers/tengine/core/event_ignitions_controller.rb
@@ -19,6 +19,8 @@ class Tengine::Core::EventIgnitionsController < ApplicationController
     @event.level = nil
     @event.occurred_at = nil
 
+    @core_event = Tengine::Core::Event.new
+
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @event }
@@ -26,27 +28,36 @@ class Tengine::Core::EventIgnitionsController < ApplicationController
   end
 
   def fire
-    begin
-      event = params[:event]
-      event_type_name = event[:event_type_name]
-      options = [:key, :source_name, :occurred_at, :level, :sender_name].inject({}) do |d, name|
-        val = event[name]
-        d[name] = val unless val.blank?
-        d
+    @core_event = Tengine::Core::Event.new(params[:event])
+    if @core_event.valid?
+      begin
+        event = params[:event]
+        event_type_name = event[:event_type_name]
+        options = [:key, :source_name, :occurred_at, :level, :sender_name].inject({}) do |d, name|
+          val = event[name]
+          d[name] = val unless val.blank?
+          d
+        end
+        yaml = event[:properties_yaml]
+        unless yaml.blank?
+          options[:properties] = YAML.load(yaml)
+        end
+        EM.run{ Tengine::Event.fire(event_type_name, options) }
+        flash[:notice] = "#{event_type_name}を発火しました"
+      rescue Exception => e
+        flash[:notice] = "#{event_type_name}の発火に失敗しました: [#{e.class.name}] #{e.message}"
       end
-      yaml = event[:properties_yaml]
-      unless yaml.blank?
-        options[:properties] = YAML.load(yaml)
-      end
-      EM.run{ Tengine::Event.fire(event_type_name, options) }
-      flash[:notice] = "#{event_type_name}を発火しました"
-    rescue Exception => e
-      flash[:notice] = "#{event_type_name}の発火に失敗しました: [#{e.class.name}] #{e.message}"
     end
 
     respond_to do |format|
-      format.html { redirect_to action: 'new' }
-      format.json { head :ok }
+      if !@core_event.errors
+        format.html { redirect_to action: 'new'}
+        format.json { head :ok }
+      else
+        @event = Tengine::Event.new(params[:event])
+        format.html { render action: 'new'}
+        format.json { head :ok }
+      end
     end
 
   end
