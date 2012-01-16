db449ddf1d4744f87aa8ba0687fa169df9f9788a
akima <akima@nautilus-technologies.com>
2012-01-16 22:46:19 +0900

    [Seleniumでジョブの正常系のテスト1001を動かせるようにする] ローカルホスト上で実行するコマンドにについては、sshだけではなくspawnやsystem、`...`でコマンドを実行できるようにしました

--- app/models/tengine/test/ssh.rb
+++ app/models/tengine/test/ssh.rb
@@ -2,7 +2,7 @@ class Tengine::Test::Ssh
   include Mongoid::Document
   include Mongoid::Timestamps
   field :host, :type => String, :default => "localhost"
-  field :local, :type => Boolean
+  field :exec_type, :type => String, :default => "ssh"
   field :user, :type => String, :default => "goku"
   field :options, :type => Hash, :default => {:password => "dragonball"}
   map_yaml_accessor :options
@@ -18,20 +18,45 @@ class Tengine::Test::Ssh
 
   def execute_command
     Timeout.timeout(self.timeout) do
-      Net::SSH.start(host, user, options) do |ssh|
-        ssh.open_channel do |ch|
-          ch.exec(command) do |ch, success|
-            unless success
-              self.error_message = "could not execute '#{command.inspect}'"
-              return
-            end
-            ch.on_data{|ch, data| self.stdout << data << "\n" }
-            ch.on_extended_data{|ch, data| self.stderr << data << "\n" }
-          end
-        end
+      case exec_type
+      when 'ssh' then execute_by_ssh
+      when 'spawn' then execute_by_spawn
+      when 'system' then execute_by_system
+      when 'backquote' then execute_by_backquote
       end
     end
   rescue => e
     self.error_message = "[#{e.class}] #{e.message}"
   end
+
+  def execute_by_ssh
+    Net::SSH.start(host, user, options) do |ssh|
+      ssh.open_channel do |ch|
+        ch.exec(command) do |ch, success|
+          unless success
+            self.error_message = "could not execute '#{command.inspect}'"
+            return
+          end
+          ch.on_data{|ch, data| self.stdout << data << "\n" }
+          ch.on_extended_data{|ch, data| self.stderr << data << "\n" }
+        end
+      end
+    end
+  end
+
+  def execute_by_spawn
+    pid = spawn(command, options || {})
+    self.error_message = "PID: #{pid}"
+  end
+
+  def execute_by_system
+    result = system(command, options || {})
+    self.error_message = "system(...) returns #{result.inspect}\nstatus:#{$?.inspect}"
+  end
+
+  def execute_by_backquote
+    self.stdout = `#{command}`
+    self.error_message = "status:#{$?.inspect}"
+  end
+
 end
