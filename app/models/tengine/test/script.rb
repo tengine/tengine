2b8585a97850e2bfc02cc47e752eb3ae3c69a152
akm <akima@nautilus-technologies.com>
2012-01-17 02:00:05 +0900

    [Seleniumでジョブの正常系のテスト1001を動かせるようにする] テスト用に任意のrubyのコードを実行できるようにしました

--- app/models/tengine/test/script.rb
+++ app/models/tengine/test/script.rb
@@ -4,4 +4,15 @@ class Tengine::Test::Script
   field :code, :type => String
   field :result, :type => String
   field :message, :type => String
+
+  validates :code, :presence => true
+
+  before_create :eval_code
+
+  def eval_code
+    self.result = eval(code)
+  rescue => e
+    self.message = "[#{e.class.name}] #{e.message}"
+  end
+
 end
