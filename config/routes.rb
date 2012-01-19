e8a7384b14807618c21fcd552a1643179571a5ac
g-morita <g-morita@nautilus-technologies.com>
2012-01-19 13:55:13 +0900

    [tengine_console の config/routes.rb に root を設定する] URLでrootにアクセスした場合、「イベント一覧」を表示するようにしました。

--- config/routes.rb
+++ config/routes.rb
@@ -125,7 +125,7 @@ TengineConsole::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'tengine/core/events#index'
 
   # See how all your routes lay out with "rake routes"
 
