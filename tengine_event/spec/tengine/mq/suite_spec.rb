ac764ef03cf429dbdd8b0346e67b40d739be6696
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2012-05-18 09:24:44 +0000

    add debug print

--- tengine_event/spec/tengine/mq/suite_spec.rb
+++ tengine_event/spec/tengine/mq/suite_spec.rb
@@ -801,6 +801,7 @@ describe Tengine::Mq::Suite do
           # netstat -an は Linux / BSD ともに有効
           # どちらかに限ればもう少し効率的な探し方はある。たとえば Linux 限定でよければ netstat -lnt ...
           y = `netstat -an | fgrep LISTEN | fgrep #{port}`
+p [$_pid, y]
           if y.lines.to_a.size >= 1
             @port = port
             return
