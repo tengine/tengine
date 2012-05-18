dd1e32d23f0af54730c582c8c6664c49170975f9
akima <akima@groovenauts.jp>
2012-05-18 17:50:59 +0900

    [リソースウォッチャのリファクタを行う] 余計なネストを削除して、インデントし直しました。

--- tengine_support/spec/tengine/support/core_ext/enumerable/map_to_hash_spec.rb
+++ tengine_support/spec/tengine/support/core_ext/enumerable/map_to_hash_spec.rb
@@ -1,33 +1,31 @@
 require File.expand_path('../../../../spec_helper', File.dirname(__FILE__))
 require "tengine/support/core_ext/enumerable/map_to_hash"
 
-describe Enumerable do
-  describe "#map_to_hash" do
+describe "Enumerable#map_to_hash" do
 
-    before(:all) do
-      person = Struct.new(:name, :age)
-      @alan = person.new("Alan", 10)
-      @becky = person.new("Becky", 19)
-      @charlie = person.new("Charlie", 50)
-    end
-
-    context "map 2 attributes to key and value of Hash" do
-      subject{ [@alan, @becky, @charlie].map_to_hash(:name, &:age) }
-      it{ should == {"Alan" => 10, "Becky" => 19, "Charlie" => 50} }
-    end
-
-    context "map an attribute and element to key and value of Hash" do
-      subject{ [@alan, @becky, @charlie].map_to_hash(:name) }
-      it{ should == {"Alan" => @alan, "Becky" => @becky, "Charlie" => @charlie} }
-    end
-
-    context "raise an UniqueKeyError when duplicated key found" do
-      it do
-        expect{
-          [@alan, @becky, @charlie, @alan].map_to_hash(:name)
-        }.to raise_error(Tengine::Support::UniqueKeyError, "duplicated key found: \"Alan\"")
-      end
-    end
-
+  before(:all) do
+    person = Struct.new(:name, :age)
+    @alan = person.new("Alan", 10)
+    @becky = person.new("Becky", 19)
+    @charlie = person.new("Charlie", 50)
   end
+
+  context "map 2 attributes to key and value of Hash" do
+    subject{ [@alan, @becky, @charlie].map_to_hash(:name, &:age) }
+    it{ should == {"Alan" => 10, "Becky" => 19, "Charlie" => 50} }
+  end
+
+  context "map an attribute and element to key and value of Hash" do
+    subject{ [@alan, @becky, @charlie].map_to_hash(:name) }
+    it{ should == {"Alan" => @alan, "Becky" => @becky, "Charlie" => @charlie} }
+  end
+
+  context "raise an UniqueKeyError when duplicated key found" do
+    it do
+      expect{
+        [@alan, @becky, @charlie, @alan].map_to_hash(:name)
+      }.to raise_error(Tengine::Support::UniqueKeyError, "duplicated key found: \"Alan\"")
+    end
+  end
+
 end
