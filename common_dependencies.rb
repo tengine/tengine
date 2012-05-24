ee488914c12a5c65ce6399b52d4b168dc22ebdcd
akima <akima@groovenauts.jp>
2012-05-24 20:01:10 +0900

    [使用するgemのバージョンを整理してできるだけ最新にする] rspec, yard, simplecovのバージョンを最新にしました。

--- common_dependencies.rb
+++ common_dependencies.rb
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def common_develooment_dependencies(s)
-  s.add_development_dependency('rake', "~> 0.9.2.2")
-  s.add_development_dependency('rspec', "~> 2.6.0")
-  s.add_development_dependency('yard', "~> 0.7.2")
   s.add_development_dependency('bundler', "~> 1.1.3")
-  s.add_development_dependency('simplecov', "~> 0.5.3")
+  s.add_development_dependency('rake', "~> 0.9.2.2")
+  s.add_development_dependency('rspec', "~> 2.10.0")
+  s.add_development_dependency('yard', "~> 0.8.1")
+  s.add_development_dependency('simplecov', "~> 0.6.4")
   s.add_development_dependency('autotest', ">= 0")
   s.add_development_dependency('rdiscount', ">= 0")
 end
