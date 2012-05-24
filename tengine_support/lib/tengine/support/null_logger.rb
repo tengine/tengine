e2d99dac57c1b1a57de804eb7b2d66295d8b4772
akm <akima@groovenauts.jp>
2012-05-25 01:06:14 +0900

    [使用するgemのバージョンを整理してできるだけ最新にする] テストでlevelを設定している箇所があったのでそれも受け付けるようにしました

--- tengine_support/lib/tengine/support/null_logger.rb
+++ tengine_support/lib/tengine/support/null_logger.rb
@@ -4,7 +4,7 @@ require 'tengine/support'
 # NullLoggerはロガーのNull Objectです。
 # Loggerと同じインタフェースを持ちますが何も動作しません。
 class Tengine::Support::NullLogger
-  %w[debug info warn error fatal].each do |log_level|
+  %w[debug info warn error fatal level level=].each do |log_level|
     class_eval("def #{log_level}(*args); end")
   end
 end
