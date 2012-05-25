cea6466b86d04832916616c0e0108d4d79134eb8
akima <akima@groovenauts.jp>
2012-05-25 16:24:31 +0900

    [使用するgemのバージョンを整理してできるだけ最新にする] インデントを揃えました

--- tengine_core/lib/tengine/rspec/extension.rb
+++ tengine_core/lib/tengine/rspec/extension.rb
@@ -40,12 +40,12 @@ module Tengine::RSpec::Extension
     end
   end
 
-    def session
-      @__session__
-    end
+  def session
+    @__session__
+  end
 
-    def tengine
-      @__tengine__
-    end
+  def tengine
+    @__tengine__
+  end
 
 end
