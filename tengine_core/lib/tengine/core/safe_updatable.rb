156113d28c4b53ddfad6600ebf8b7bf2796c857d
akima <akima@groovenauts.jp>
2012-05-25 11:43:45 +0900

    [使用するgemのバージョンを整理してできるだけ最新にする] ActiveSupport::ConcernをextendしたモジュールからInstanceMethodsモジュールを削除したので、インデントし直しました。

--- tengine_core/lib/tengine/core/safe_updatable.rb
+++ tengine_core/lib/tengine/core/safe_updatable.rb
@@ -3,35 +3,35 @@ require 'tengine/core'
 module Tengine::Core::SafeUpdatable
   extend ActiveSupport::Concern
 
-    def update_in_safe_mode(collection, selector, document, opts=nil)
-      options = { :upsert => false, :multiple => false }
-      options.update(opts) if opts
+  def update_in_safe_mode(collection, selector, document, opts=nil)
+    options = { :upsert => false, :multiple => false }
+    options.update(opts) if opts
 
-      options = options.merge({ :safe => safemode(collection, 10240) })
+    options = options.merge({ :safe => safemode(collection, 10240) })
 
-      max_retries = 60
-      retries = 0
-      begin
-        # Return a Hash containing the last error object if running safe mode.
-        # Otherwise, returns true
-        result = collection.driver.update(selector, document, options)
-      rescue Mongo::ConnectionFailure, Mongo::OperationFailure => ex
-        case ex when Mongo::OperationFailure then
-          raise ex unless ex.message =~ /wtimeout/
-        end
-        retries += 1
-        raise ex if retries > max_retries
-        Tengine.logger.debug "retrying due to mongodb error #{ex.inspect}, #{retries} times."
-        sleep 0.5
-        retry
+    max_retries = 60
+    retries = 0
+    begin
+      # Return a Hash containing the last error object if running safe mode.
+      # Otherwise, returns true
+      result = collection.driver.update(selector, document, options)
+    rescue Mongo::ConnectionFailure, Mongo::OperationFailure => ex
+      case ex when Mongo::OperationFailure then
+        raise ex unless ex.message =~ /wtimeout/
       end
+      retries += 1
+      raise ex if retries > max_retries
+      Tengine.logger.debug "retrying due to mongodb error #{ex.inspect}, #{retries} times."
+      sleep 0.5
+      retry
     end
+  end
 
-    def safemode(collection, wtimeout=61440)
-      res = true
-      case collection.driver.db.connection when Mongo::ReplSetConnection then
-        res = { :w => "majority", :wtimeout => wtimeout, }
-      end
-      res
+  def safemode(collection, wtimeout=61440)
+    res = true
+    case collection.driver.db.connection when Mongo::ReplSetConnection then
+      res = { :w => "majority", :wtimeout => wtimeout, }
     end
+    res
+  end
 end
