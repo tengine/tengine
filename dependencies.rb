54d4c451c20bb76911eca8655dd47eede6c283ef
akima <akima@groovenauts.jp>
2012-05-28 17:59:52 +0900

    [EC2やwakameの代わりにVMwareなどのクラウドコントローラを使えるようにする] tengine_resource_ec2 と tengine_resource_wakame についての依存関係もdependencies.rbに記述しました。

--- dependencies.rb
+++ dependencies.rb
@@ -14,11 +14,13 @@ end
 PackageDef = Struct.new(:package_type, :name, :dependencies)
 
 PACKAGES = [
-  PackageDef.new(:gem, 'tengine_support'  , %w[]),
-  PackageDef.new(:gem, 'tengine_event'    , %w[tengine_support]),
-  PackageDef.new(:gem, 'tengine_core'     , %w[tengine_support tengine_event]),
-  PackageDef.new(:gem, 'tengine_resource' , %w[tengine_support tengine_event tengine_core]),
-  PackageDef.new(:gem, 'tengine_job'      , %w[tengine_support tengine_event tengine_core tengine_resource]),
-  PackageDef.new(:gem, 'tengine_job_agent', %w[tengine_support tengine_event]),
-  PackageDef.new(:rails, 'tengine_ui'     , %w[tengine_support tengine_event tengine_core tengine_resource tengine_job]),
+  PackageDef.new(:gem, 'tengine_support'        , %w[]),
+  PackageDef.new(:gem, 'tengine_event'          , %w[tengine_support]),
+  PackageDef.new(:gem, 'tengine_core'           , %w[tengine_support tengine_event]),
+  PackageDef.new(:gem, 'tengine_resource'       , %w[tengine_support tengine_event tengine_core]),
+  PackageDef.new(:gem, 'tengine_resource_ec2'   , %w[tengine_support tengine_event tengine_core tengine_resource]),
+  PackageDef.new(:gem, 'tengine_resource_wakame', %w[tengine_support tengine_event tengine_core tengine_resource tengine_resource_ec2]),
+  PackageDef.new(:gem, 'tengine_job'            , %w[tengine_support tengine_event tengine_core tengine_resource]),
+  PackageDef.new(:gem, 'tengine_job_agent'      , %w[tengine_support tengine_event]),
+  PackageDef.new(:rails, 'tengine_ui'           , %w[tengine_support tengine_event tengine_core tengine_resource tengine_job]),
 ]
