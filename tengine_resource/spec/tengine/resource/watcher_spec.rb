9dba05da5d437d06e14a903d3a19a34f0cd1649e
akima <akima@groovenauts.jp>
2012-05-31 15:39:00 +0900

    [EC2やwakameの代わりにVMwareなどのクラウドコントローラを使えるようにする#2] プロバイダが全く存在しない場合のテストを追加する前にbeforeを分割しました

--- tengine_resource/spec/tengine/resource/watcher_spec.rb
+++ tengine_resource/spec/tengine/resource/watcher_spec.rb
@@ -9,32 +9,38 @@ describe Tengine::Resource::Watcher do
   describe :find_providers do
     before do
       Tengine::Resource::Provider.delete_all
-      @provider1 = TestProvider1.create!(:name => "test1", :connection_settings => {:foo => "FOO", :bar => "BAR"})
-      @provider2 = TestProvider1.create!(:name => "test2", :connection_settings => {:baz => "BAZ"})
     end
 
-    context "valid" do
-      subject{ Tengine::Resource::Watcher.new.find_providers }
-
-      its(:length){ should == 2 }
-      its(:first){ should be_a(TestProvider1) }
-      its(:last){ should be_a(TestProvider1) }
-      it{ subject.first.id.should == @provider1.id }
-      it{ subject.last.id.should == @provider2.id }
-    end
-
-    context "with provider class in unloaded package" do
-
+    context "providers exist" do
       before do
-        @provider_class_name = "Tengine::ResourceEc2::Provider"
-        Tengine::Resource::Provider.collection.tap do |c|
-          c.insert({:_id => BSON::ObjectId.new, :_type => @provider_class_name, :name => "tengine@ec2-tokyo",
-            :connection_settings => {"access_key_file" => "~/.ec2/access_key", "secret_access_key_file" => "~/.ec2/secret_access_key"}})
-        end
+        @provider1 = TestProvider1.create!(:name => "test1", :connection_settings => {:foo => "FOO", :bar => "BAR"})
+        @provider2 = TestProvider1.create!(:name => "test2", :connection_settings => {:baz => "BAZ"})
       end
 
-      subject{ expect{ Tengine::Resource::Watcher.new.find_providers } }
-      it{ should raise_error(NameError, "class not found: #{@provider_class_name}")}
+      context "valid" do
+        subject{ Tengine::Resource::Watcher.new.find_providers }
+
+        its(:length){ should == 2 }
+        its(:first){ should be_a(TestProvider1) }
+        its(:last){ should be_a(TestProvider1) }
+        it{ subject.first.id.should == @provider1.id }
+        it{ subject.last.id.should == @provider2.id }
+      end
+
+      context "with provider class in unloaded package" do
+
+        before do
+          @provider_class_name = "Tengine::ResourceEc2::Provider"
+          Tengine::Resource::Provider.collection.tap do |c|
+            c.insert({:_id => BSON::ObjectId.new, :_type => @provider_class_name, :name => "tengine@ec2-tokyo",
+                :connection_settings => {"access_key_file" => "~/.ec2/access_key", "secret_access_key_file" => "~/.ec2/secret_access_key"}})
+          end
+        end
+
+        subject{ expect{ Tengine::Resource::Watcher.new.find_providers } }
+        it{ should raise_error(NameError, "class not found: #{@provider_class_name}")}
+      end
+
     end
   end
 
