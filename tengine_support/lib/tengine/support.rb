956df778d964ef4856aa62c01480d149cd830968
taigou <taigou@gmail.com>
2012-06-04 23:53:13 +0900

    [パフォーマンス改善策の実施#1]すでにlib/tengine/support/mongoid.rbがあるので、インデックス生成の機能もあわせて記述するようにしました

--- tengine_support/lib/tengine/support.rb
+++ tengine_support/lib/tengine/support.rb
@@ -4,5 +4,5 @@ module Tengine::Support
   autoload :YamlWithErb,  'tengine/support/yaml_with_erb'
   autoload :NullLogger,   'tengine/support/null_logger'
   autoload :Config,       'tengine/support/config'
-  autoload :MongoIndex,   'tengine/support/mongo_index'
+  autoload :Mongoid,      'tengine/support/mongoid'
 end
--- tengine_support/spec/tengine/support/mongoid_spec.rb
+++ tengine_support/spec/tengine/support/mongoid_spec.rb
@@ -34,3 +34,51 @@ describe Mongoid::Document do
 
   end
 end
+
+describe "Tengine::Support::Mongoid" do
+  describe :create_indexes do
+
+    context "when model exist in subdirectories" do
+
+      context "when the model is namespaced" do
+        module Twitter
+          class Follow
+            include Mongoid::Document
+          end
+        end
+
+        let(:files) { ["/lib/twitter/follow.rb"] }
+        let(:conf) { stub }
+
+        before do
+          Dir.should_receive(:glob).with("/lib/**/*.rb").and_return(files)
+          Mongoid.should_receive(:configure).and_return(conf)
+        end
+
+        it "loads the model with the namespacing" do
+          Twitter::Follow.should_receive(:create_indexes).once
+          Tengine::Support::Mongoid.create_indexes("/lib")
+        end
+      end
+
+      context "when the model is not namespaced" do
+        class Unfollow; include Mongoid::Document end
+
+        let(:files) { ["/lib/twitter/unfollow.rb"] }
+        let(:conf) { stub }
+
+        before do
+          Dir.should_receive(:glob).with("/lib/**/*.rb").and_return(files)
+          Mongoid.should_receive(:configure).and_return(conf)
+        end
+
+        it "loads the model with the namespacing" do
+          Unfollow.should_receive(:create_indexes).once
+          Tengine::Support::Mongoid.create_indexes("/lib")
+        end
+      end
+    end
+  end
+
+end
+
