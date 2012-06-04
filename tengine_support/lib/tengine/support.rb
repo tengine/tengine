22706d95c63ee9f9d7b9fcf735e4254ef9f3d47b
taigou <taigou@gmail.com>
2012-06-04 23:15:38 +0900

    [パフォーマンス改善策の実施#1]MongoidのRails::Mongoidを参考に実装しました

--- tengine_support/lib/tengine/support.rb
+++ tengine_support/lib/tengine/support.rb
@@ -1,7 +1,8 @@
 require 'tengine_support'
 
 module Tengine::Support
-  autoload :YamlWithErb, 'tengine/support/yaml_with_erb'
-  autoload :NullLogger, 'tengine/support/null_logger'
-  autoload :Config, 'tengine/support/config'
+  autoload :YamlWithErb,  'tengine/support/yaml_with_erb'
+  autoload :NullLogger,   'tengine/support/null_logger'
+  autoload :Config,       'tengine/support/config'
+  autoload :MongoIndex,   'tengine/support/mongo_index'
 end
---
+++ tengine_support/lib/tengine/support/mongo_index.rb
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+require 'tengine/support'
+require 'mongoid'
+
+module Tengine::Support::MongoIndex
+  extend self
+
+  # Create indexes for each model given provided file_path directory and
+  # the class is not embedded.
+  #
+  def create_indexes(pattern, connect_to=nil)
+    connect_to ||= 'localhost:27017/tengine_production'
+    host, port, db_name = connect_to.split('/').map{|s| s.split(':')}.flatten
+
+    Mongoid.configure do |c|
+      c.master = Mongo::Connection.new(host, port).db(db_name)
+    end
+
+    Dir.glob("#{pattern}/**/*.rb").each do |file|
+      begin
+        model = determine_model(file)
+      rescue => e
+        $stderr.puts(%Q{Failed to determine model from #{file}:
+            #{e.class}:#{e.message}
+            #{e.backtrace.join("\n")}
+          })
+      end
+
+      if model
+        model.create_indexes
+        $stdout.puts "Generated indexes for #{model}"
+      else
+        $stdout.puts "Not a Mongoid parent model: #{file}"
+      end
+    end
+  end
+
+  def determine_model(file)
+    if file =~ /lib\/(.*).rb$/
+      model_path = $1.split('/')
+      begin
+        parts = model_path.map { |path| path.camelize }
+        name = parts.join('::')
+        klass = name.constantize
+      rescue NameError, LoadError => e
+        klass = parts.last.constantize
+      end
+    end
+    return klass if klass.ancestors.include?(::Mongoid::Document) && !klass.embedded
+  end
+end
