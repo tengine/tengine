800f2cc3521e0b2c7b3eb59a985201880aa9af68
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2012-11-12 20:50:40 +0900

    TengineRailsPlugin::Progress をモデルに Mixin して使うモジュールとして定義しなおしました。

--- tengine_rails_plugin/lib/tengine_rails_plugin/progress.rb
+++ tengine_rails_plugin/lib/tengine_rails_plugin/progress.rb
@@ -3,46 +3,69 @@
 require "eventmachine"
 
 module TengineRailsPlugin
-  class Progress
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    field :name, type: String
-    # 0: sended, 1: send_failure, 2: starting, 3: starting_failure, 4: error, 5: success
-    field :status_cd, type: Integer
-    field :logs, type: Array
+  module Progress
+    STATUS_TYPES = SelectableAttr::Enum.new do
+      name "Tengine進捗状態"
+      entry 0, :initial,          "初期状態"
+      entry 1, :sended,           "送信済み"
+      entry 2, :send_failure,     "送信失敗"
+      entry 3, :starting,         "実行中"
+      entry 4, :starting_failure, "実行開始失敗"
+      entry 5, :error,            "実行失敗"
+      entry 6, :success,          "実行成功"
+    end
 
     EVENT_NAME_PREFIX = "tengine.progress."
-    def self.fire(batch_name, options={}, &block)
-      progress = self.create!(name: batch_name, status_cd: 0,
-                              logs: [ "#{Time.zone.now.rfc2822} sending the event to start" ])
+
+    def event_name(name)
+      "#{EVENT_NAME_PREFIX}#{name}"
+    end
+
+    def add_logs(*logs)
+      if self.respond_to?(:tengine_logs) and self.respond_to?(:tengine_logs=)
+        self.tengine_logs ||= []
+        self.tengine_logs += logs
+      end
+    end
+
+    def set_status(status)
+      if self.respond_to?(:status_cd=)
+        self.status_cd = status
+      end
+    end
+
+    def fire(batch_name, options={}, &block)
+      add_logs("#{Time.zone.now.rfc2822} sending the event to start")
+      set_status(1)
       begin
-        properties = { batch_id: progress.id, batch_name: batch_name }.update(options || {})
+        properties = { batch_id: self.id, batch_name: batch_name }.update(options || {})
         EventMachine.run do
-          Tengine::Event.fire((EVENT_NAME_PREFIX+batch_name).to_sym, properties: properties)
+          Tengine::Event.fire(event_name(batch_name), properties: properties)
         end
       rescue Exception
-        progress.status_cd = 1
-        progress.logs << "#{Time.zone.now.rfc2822} #{$!.class.name} #{$!.message}\n  " << $!.backtrace.join("\n  ")
-        progress.save!
+        add_logs("#{Time.zone.now.rfc2822} #{$!.class.name} #{$!.message}\n  " + $!.backtrace.join("\n  "))
+        set_status(2)
+        self.save!
         raise
       end
     end
 
-    def self.run(event)
-      progress = self.find(event[:batch_id])
-      progress.logs << "#{Time.zone.now.rfc2822} Starting"
-      progress.status_cd = 2
+    def self.run(model, event)
+      progress = model.find(event[:batch_id])
+      progress.extend(self)
+      progress.add_logs("#{Time.zone.now.rfc2822} Starting")
+      progress.set_status(3)
       progress.save!
       begin
         yield(progress) if block_given?
       rescue
-        progress.logs << "#{Time.zone.now.rfc2822} [#{$!.class.name}] #{$!.message}\n  " << $!.backtrace.join("\n  ")
-        progress.status_cd = 4
+        progress.add_logs("#{Time.zone.now.rfc2822} [#{$!.class.name}] #{$!.message}\n  " + $!.backtrace.join("\n  "))
+        progress.set_status(5)
         progress.save!
         raise
       else
-        progress.status_cd = 5
-        progress.logs << "#{Time.zone.now.rfc2822} SUCCESS"
+        progress.add_logs("#{Time.zone.now.rfc2822} SUCCESS")
+        progress.set_status(6)
         progress.save!
       end
     end
