df8ae9ba688a48999faee3efc140a84b7809a884
akima <akm2000@gmail.com>
2013-02-13 20:00:02 +0900

    Tengine::Job::Template::SshJobを生成し、必要な情報としてKillingの属性を定義するようにしました。

---
+++ tengine_job/lib/tengine/job/template/ssh_job.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+require 'tengine/job/template'
+
+# ルートジョブネットを他のジョブネット内に展開するための特殊なテンプレート用Vertex。
+class Tengine::Job::Template::SshJob < Tengine::Job::Template::NamedVertex
+
+  # Tengine::Job::Runtime::SshJobでも必要な属性
+  module Settings
+    extend ActiveSupport::Concern
+
+    included do
+      require 'tengine/core'
+      include Tengine::Core::CollectionAccessible
+
+      field :killing_signals, :type => Array # 強制停止時にプロセスに送るシグナルの配列
+      array_text_accessor :killing_signals
+
+      field :killing_signal_interval, :type => Integer # 強制停止時にkilling_signalsで定義されるシグナルを順次送信する間隔。
+    end
+  end
+
+  include Settings
+
+  DEFAULT_KILLING_SIGNAL_INTERVAL = 5
+
+  def actual_killing_signals
+    killing_signals ? killing_signals :
+      (parent ? parent.actual_killing_signals : ['KILL'])
+  end
+
+  def actual_killing_signal_interval
+    killing_signals ? killing_signal_interval :
+      (parent ? parent.actual_killing_signal_interval : DEFAULT_KILLING_SIGNAL_INTERVAL)
+  end
+
+end
