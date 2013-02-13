61111265b73476987bd28c5ff2fc6f74a445406f
akima <akm2000@gmail.com>
2013-02-13 20:02:09 +0900

    Connectableの内容をSshJobに統合しました

--- tengine_job/lib/tengine/job/template/ssh_job.rb
+++ tengine_job/lib/tengine/job/template/ssh_job.rb
@@ -12,10 +12,39 @@ class Tengine::Job::Template::SshJob < Tengine::Job::Template::NamedVertex
       require 'tengine/core'
       include Tengine::Core::CollectionAccessible
 
+      field :server_name    , :type => String # 接続先となるサーバ名。Tengine::Resource::Server#name を指定します
+      field :credential_name, :type => String # 接続時に必要な認証情報。Tengine::Resource::Credential#name を指定します
+
       field :killing_signals, :type => Array # 強制停止時にプロセスに送るシグナルの配列
       array_text_accessor :killing_signals
 
       field :killing_signal_interval, :type => Integer # 強制停止時にkilling_signalsで定義されるシグナルを順次送信する間隔。
+    end
+
+    def actual_credential_name
+      credential_name || (parent ? parent.actual_credential_name : nil)
+    end
+
+    def actual_server_name
+      server_name || (parent ? parent.actual_server_name : nil)
+    end
+
+    def actual_credential
+      key = actual_credential_name
+      return nil if key.blank?
+      result = Tengine::Resource::Credential.where({:name => key}).first
+      # TODO 使用する例外クラスはこれで良いのか検討
+      raise Mongoid::Errors::DocumentNotFound.new(Tengine::Resource::Credential, key, []) unless result
+      result
+    end
+
+    def actual_server
+      key = actual_server_name
+      return nil if key.blank?
+      result = Tengine::Resource::Server.where({:name => key}).first
+      # TODO 使用する例外クラスはこれで良いのか検討
+      raise Mongoid::Errors::DocumentNotFound.new(Tengine::Resource::Server, key, []) unless result
+      result
     end
   end
 
