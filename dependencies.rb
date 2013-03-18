62b64d719f3c66d50d77619fc643f410d0513391
秋間 武志 <akima@akima-no-MacBook-Air.local>
2013-03-18 22:46:29 +0900

    tengine_job が tengine_job_agent に依存するのでそのようにしました。

--- dependencies.rb
+++ dependencies.rb
@@ -27,7 +27,7 @@ PACKAGES = [
   PackageDef.new(:gem, 'tengine_resource'       , %w[tengine_support tengine_event tengine_core]),
   PackageDef.new(:gem, 'tengine_resource_ec2'   , %w[tengine_support tengine_event tengine_core tengine_resource]),
   PackageDef.new(:gem, 'tengine_resource_wakame', %w[tengine_support tengine_event tengine_core tengine_resource tengine_resource_ec2]),
-  PackageDef.new(:gem, 'tengine_job'            , %w[tengine_support tengine_event tengine_core tengine_resource tengine_resource_ec2]),
   PackageDef.new(:gem, 'tengine_job_agent'      , %w[tengine_support tengine_event]),
+  PackageDef.new(:gem, 'tengine_job'            , %w[tengine_support tengine_event tengine_core tengine_resource tengine_resource_ec2 tengine_job_agent]),
   PackageDef.new(:rails, 'tengine_ui'           , %w[tengine_support tengine_event tengine_core tengine_resource tengine_resource_ec2 tengine_resource_wakame tengine_job]),
 ]
