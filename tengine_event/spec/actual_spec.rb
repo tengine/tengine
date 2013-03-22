56c7e96f290ab955fb0490433f2fb14400bef4d3
akima <akm2000@gmail.com>
2013-03-22 10:50:45 +0900

    fix indent

--- tengine_event/spec/actual_spec.rb
+++ tengine_event/spec/actual_spec.rb
@@ -53,34 +53,34 @@ describe "tengine_event" do
 
   # 環境によって失敗する割合が異なるので、多めに実行しています。 10%の環境もあれば50%の環境もあります。
   (ENV['REPEAT'] || 20).times do |idx|
-  context "publisher is in another process ##{idx}" do
-    let(:timeout){ 10 }
-    let(:buffer){ [] }
-    let(:suite ){ Tengine::Mq::Suite.new }
+    context "publisher is in another process ##{idx}" do
+      let(:timeout){ 10 }
+      let(:buffer){ [] }
+      let(:suite ){ Tengine::Mq::Suite.new }
 
-    before do
-      Tengine.logger = logger
-      EM.run do
-        # EM.next_tick do
-        #   puts "now waiting 2 events in #{timeout} seconds."
-        # end
-        EM.add_timer(timeout) { suite.stop } # timeoutを設定
+      before do
+        Tengine.logger = logger
+        EM.run do
+          # EM.next_tick do
+          #   puts "now waiting 2 events in #{timeout} seconds."
+          # end
+          EM.add_timer(timeout) { suite.stop } # timeoutを設定
 
-        suite.subscribe do |header, payload|
-          header.ack
-          hash = JSON.parse(payload)
-          # puts hash.inspect
-          buffer << hash["event_type_name"]
-          suite.stop if buffer.length >= 2
+          suite.subscribe do |header, payload|
+            header.ack
+            hash = JSON.parse(payload)
+            # puts hash.inspect
+            buffer << hash["event_type_name"]
+            suite.stop if buffer.length >= 2
+          end
+          cmd = File.expand_path("../actual_publisher1.rb", __FILE__)
+          system(cmd)
         end
-        cmd = File.expand_path("../actual_publisher1.rb", __FILE__)
-        system(cmd)
+      end
+
+      it "receives foo and bar" do
+        buffer.should =~ %w[foo bar]
       end
     end
-
-    it "receives foo and bar" do
-      buffer.should =~ %w[foo bar]
-    end
-  end
   end
 end
--- tengine_event/spec/amqp_spec.rb
+++ tengine_event/spec/amqp_spec.rb
@@ -27,115 +27,115 @@ describe "amqp" do
 
   # 環境によって失敗する割合が異なるので、多めに実行しています。 10%の環境もあれば50%の環境もあります。
   (ENV['REPEAT'] || 10).times do |idx|
-  [
-    "actual_publisher1.rb",
-    "actual_publisher2.rb",
-    "actual_publisher2.rb nest",
-  ].each do |publisher_command|
+    [
+      "actual_publisher1.rb",
+      "actual_publisher2.rb",
+      "actual_publisher2.rb nest",
+    ].each do |publisher_command|
 
-    context "##{idx} publisher is in another process with #{publisher_command}" do
-      let(:timeout){ 10 }
-      let(:buffer){ [] }
+      context "##{idx} publisher is in another process with #{publisher_command}" do
+        let(:timeout){ 10 }
+        let(:buffer){ [] }
 
-      let(:config) do
-        {
-          :sender                 => {
-            :keep_connection      => false,
-            :retry_interval       => 1,  # in seconds
-            :retry_count          => 30,
-          },
-          :connection             => {
-            :user                 => 'guest',
-            :pass                 => 'guest',
-            :vhost                => '/',
-            :logging              => false,
-            :insist               => false,
-            :host                 => 'localhost',
-            :port                 => 5672,
-            :auto_reconnect_delay => 1, # in seconds
-          },
-          :channel                => {
-            :prefetch             => 1,
-            :auto_recovery        => true,
-          },
-          :exchange               => {
-            :name                 => 'tengine_event_exchange',
-            :type                 => :direct,
-            :passive              => false,
-            :durable              => true,
-            :auto_delete          => false,
-            :internal             => false,
-            :nowait               => false,
-            :publish              => {
-              :content_type       => "application/json", # RFC4627
-              :persistent         => true,
+        let(:config) do
+          {
+            :sender                 => {
+              :keep_connection      => false,
+              :retry_interval       => 1,  # in seconds
+              :retry_count          => 30,
             },
-          },
-          :queue                  => {
-            :name                 => 'tengine_event_queue',
-            :passive              => false,
-            :durable              => true,
-            :auto_delete          => false,
-            :exclusive            => false,
-            :nowait               => false,
-            :subscribe            => {
-              :ack                => true,
-              :nowait             => false,
-              :confirm            => nil,
+            :connection             => {
+              :user                 => 'guest',
+              :pass                 => 'guest',
+              :vhost                => '/',
+              :logging              => false,
+              :insist               => false,
+              :host                 => 'localhost',
+              :port                 => 5672,
+              :auto_reconnect_delay => 1, # in seconds
             },
-          },
-        }
-      end
+            :channel                => {
+              :prefetch             => 1,
+              :auto_recovery        => true,
+            },
+            :exchange               => {
+              :name                 => 'tengine_event_exchange',
+              :type                 => :direct,
+              :passive              => false,
+              :durable              => true,
+              :auto_delete          => false,
+              :internal             => false,
+              :nowait               => false,
+              :publish              => {
+                :content_type       => "application/json", # RFC4627
+                :persistent         => true,
+              },
+            },
+            :queue                  => {
+              :name                 => 'tengine_event_queue',
+              :passive              => false,
+              :durable              => true,
+              :auto_delete          => false,
+              :exclusive            => false,
+              :nowait               => false,
+              :subscribe            => {
+                :ack                => true,
+                :nowait             => false,
+                :confirm            => nil,
+              },
+            },
+          }
+        end
 
-      before do
-        EM.run do
-          # EM.next_tick do
-          #   puts "now waiting 2 events in #{timeout} seconds."
-          # end
-          EM.add_timer(timeout) { EM.stop } # timeoutを設定
+        before do
+          EM.run do
+            # EM.next_tick do
+            #   puts "now waiting 2 events in #{timeout} seconds."
+            # end
+            EM.add_timer(timeout) { EM.stop } # timeoutを設定
 
-          AMQP.connect(config[:connection]) do |conn|
-            id = AMQP::Channel.next_channel_id
-            AMQP::Channel.new(conn, id, config[:channel]) do |ch|
+            AMQP.connect(config[:connection]) do |conn|
+              id = AMQP::Channel.next_channel_id
+              AMQP::Channel.new(conn, id, config[:channel]) do |ch|
 
-              cfg = config[:exchange].dup
-              name = cfg.delete :name
-              type = cfg.delete :type
-              cfg.delete :publish # not needed here
-              AMQP::Exchange.new ch, type.intern, name, cfg do |xchg|
-
-                cfg = config[:queue].dup
+                cfg = config[:exchange].dup
                 name = cfg.delete :name
-                ch.queue name, cfg do |que|
-                  que.bind xchg, :nowait => false do
+                type = cfg.delete :type
+                cfg.delete :publish # not needed here
+                AMQP::Exchange.new ch, type.intern, name, cfg do |xchg|
 
-                    opts = config[:queue][:subscribe]
-                    que.subscribe opts do |h, b|
-                      h.ack
-                      # puts b
-                      hash = JSON.parse(b)
-                      buffer << hash["event_type_name"]
-                      EM.stop if buffer.length > 1
+                  cfg = config[:queue].dup
+                  name = cfg.delete :name
+                  ch.queue name, cfg do |que|
+                    que.bind xchg, :nowait => false do
+
+                      opts = config[:queue][:subscribe]
+                      que.subscribe opts do |h, b|
+                        h.ack
+                        # puts b
+                        hash = JSON.parse(b)
+                        buffer << hash["event_type_name"]
+                        EM.stop if buffer.length > 1
+                      end
+
                     end
-
                   end
-                end
 
+                end
               end
+              logger.debug("#{__FILE__}##{__LINE__}")
             end
             logger.debug("#{__FILE__}##{__LINE__}")
-          end
-          logger.debug("#{__FILE__}##{__LINE__}")
 
-          Process.spawn(File.expand_path("../#{publisher_command}", __FILE__))
+            Process.spawn(File.expand_path("../#{publisher_command}", __FILE__))
+          end
+        end
+
+        it "receives foo and bar" do
+          buffer.should == %w[foo bar]
         end
       end
-
-      it "receives foo and bar" do
-        buffer.should == %w[foo bar]
-      end
     end
-  end
   end
 
 end
